1772cd40336523914068d0b4fbd898c4b1b551e9
Tair Sabirgaliev <tair.sabirgaliev@gmail.com>
2016-08-05 10:43:02 +0600

    initial commit

---
+++ kcell-demo-process-app/src/main/java/kz/kcell/bpm/KcellDemoApplication.java
@@ -0,0 +1,9 @@
+package kz.kcell.bpm;
+
+import org.camunda.bpm.application.ProcessApplication;
+import org.camunda.bpm.application.impl.ServletProcessApplication;
+
+@ProcessApplication("Kcell Demo App")
+public class KcellDemoApplication extends ServletProcessApplication {
+
+}
---
+++ kcell-demo-process-app/src/test/java/kz/kcell/bpm/KcellDemoApplicationTest.java
@@ -0,0 +1,43 @@
+package kz.kcell.bpm;
+
+import org.camunda.bpm.engine.identity.User;
+import org.camunda.bpm.engine.task.Task;
+import org.camunda.bpm.engine.test.Deployment;
+import org.camunda.bpm.engine.test.ProcessEngineTestCase;
+import org.junit.Assert;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class KcellDemoApplicationTest extends ProcessEngineTestCase {
+
+    // TODO: Set the Demo User Email Address
+    private static final String EMAIL = "demo@example.org";
+
+
+    @Deployment(resources = "kcellDemoProcess.bpmn")
+    public void testSimpleProcess() {
+        Map<String, Object> variables = new HashMap<String, Object>();
+        variables.put("city", "Berlin");
+        variables.put("country", "Germany");
+
+        // Create the user that will be informed on assignment
+        User newUser = identityService.newUser("demo");
+        newUser.setEmail(EMAIL);
+        identityService.saveUser(newUser);
+
+        runtimeService.startProcessInstanceByKey("KcellDemoProcess", variables);
+
+        Task task = taskService.createTaskQuery().singleResult();
+        Assert.assertNotNull(task);
+
+        int temperature = Integer.parseInt( taskService.getVariable(task.getId(), "temperature").toString() );
+        if(temperature >= 18) {
+            Assert.assertEquals("PackWarmTask", task.getTaskDefinitionKey());
+        } else {
+            Assert.assertEquals("PackColdTask", task.getTaskDefinitionKey());
+        }
+
+    }
+
+}
