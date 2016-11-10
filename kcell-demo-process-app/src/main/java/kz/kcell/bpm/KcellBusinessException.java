bbf4023923379d57a17d0c640c7f2bb6918b236f
nmuldashev <nurlan@muldashev.kz>
2016-11-10 14:56:30 +0600

    Revision first commit

---
+++ kcell-demo-process-app/src/main/java/kz/kcell/bpm/KcellBusinessException.java
@@ -0,0 +1,7 @@
+package kz.kcell.bpm;
+
+/**
+ * Created by meta4 on 04.11.2016.
+ */
+public class KcellBusinessException {
+}
--- kcell-demo-process-app/src/main/java/kz/kcell/bpm/KcellDemoApplication.java
+++ kcell-demo-process-app/src/main/java/kz/kcell/bpm/KcellDemoApplication.java
@@ -1,9 +1,19 @@
 package kz.kcell.bpm;
 
+import org.camunda.bpm.application.PostDeploy;
 import org.camunda.bpm.application.ProcessApplication;
 import org.camunda.bpm.application.impl.ServletProcessApplication;
+import org.camunda.bpm.engine.ProcessEngine;
 
 @ProcessApplication("Kcell Demo App")
 public class KcellDemoApplication extends ServletProcessApplication {
 
+    @PostDeploy
+    public void startFirstProcess(ProcessEngine processEngine) {
+        this.createUsers(processEngine);
+    }
+
+    private void createUsers(ProcessEngine processEngine) {
+        (new DemoDataGenerator()).createUsers(processEngine);
+    }
 }
