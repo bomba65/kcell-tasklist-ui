8f8661d4bf91c77aa357994358d700c60da2370d
Tair Sabirgaliev <tair.sabirgaliev@gmail.com>
2017-02-15 15:40:24 +0600

    KWMS-184

---
+++ docker-build.sh
@@ -0,0 +1,17 @@
+#/bin/bash
+
+set -e
+
+DOCKER_COMPOSE_YML=$1
+
+if [ -z "$DOCKER_COMPOSE_YML"]; then
+	DOCKER_COMPOSE_YML="docker-compose.yml"
+fi
+
+BASEDIR=`pwd`
+
+cd "$BASEDIR/asset-management"
+./gradlew build
+
+cd "$BASEDIR"
+docker-compose -f "$DOCKER_COMPOSE_YML" build
