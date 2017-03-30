e1005704df26647d034b6c4cd3b2f7edba0e36c4
Tair Sabirgaliev <tair.sabirgaliev@gmail.com>
2017-03-30 12:09:48 +0600

    include LDAP authentication module in build process

--- docker-build.sh
+++ docker-build.sh
@@ -16,5 +16,10 @@ cd "$BASEDIR/asset-management"
 cd "$BASEDIR/kcell-demo-process-app"
 ./mvnw package
 
+cd "$BASEDIR/ldap-authentication"
+./mvnw package
+
+cp "$BASEDIR/ldap-authentication/target/ldap-authentication-7.6.0.jar" "$BASEDIR/kcell-camunda/authentication-module/ldap-authentication-7.6.0.jar"
+
 cd "$BASEDIR"
 docker-compose -f "$DOCKER_COMPOSE_YML" build
