2759a051d3dc5da516030f3ef9e70f61ee604fdb
Nurlan Muldashev <nurlan.muldashev@metaphor.kz>
2017-04-19 18:44:04 +0600

    KWMS-249: Поменял отображение для всех типов оборудования и connection

--- kcell-demo-process-app/src/main/java/kz/kcell/bpm/assignments/ApproveMaterialsListByRegionGroupsHeadAssignmentHandler.java
+++ kcell-demo-process-app/src/main/java/kz/kcell/bpm/assignments/ApproveMaterialsListByRegionGroupsHeadAssignmentHandler.java
@@ -17,13 +17,13 @@ public class ApproveMaterialsListByRegionGroupsHeadAssignmentHandler implements 
         String reason = delegateTask.getVariable("reason").toString();
         if (reason != null) {
             if (reason.equals("1")) {
-                delegateTask.addCandidateUsers(Collections.emptyList());
+                delegateTask.addCandidateUsers(Arrays.asList("Kerey.Zatilda@kcell.kz", "Kanat.Kulmukhambetov@kcell.kz"));
             } else if (reason.equals("2")) {
                 delegateTask.addCandidateUsers(Arrays.asList("Nurzhan.Mynbayev@kcell.kz", "Asset.Rashitov@kcell.kz", "Vladimir.Yefanov@kcell.kz", "Aslan.Shalov@kcell.kz", "Shyngys.Kassabekov@kcell.kz"));
             } else if (reason.equals("3")) {
                 delegateTask.addCandidateUsers(Arrays.asList("Alexey.Khudaev@kcell.kz", "Evgeniy.Semenov@kcell.kz", "Sergey.Chekh@kcell.kz", "Sergey.Lee@kcell.kz", "Vladimir.Yefanov@kcell.kz", "Yevgeniy.Elunin@kcell.kz", "Yermek.Tanabekov@kcell.kz", "Andrei.Lugovoy@kcell.kz", "Alexey.Kolesnikov@kcell.kz"));
             } else if (reason.equals("4")) {
-                delegateTask.addCandidateUsers(Collections.emptyList());
+                delegateTask.addCandidateUsers(Arrays.asList("Askar.Bekmurzayev@kcell.kz", "Temirlan.Kaliyev@kcell.kz"));
             }
         }
     }
