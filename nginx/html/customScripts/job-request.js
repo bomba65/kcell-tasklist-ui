981b3fecd709a894e03a2bc115a3acb9aa1424ce
Nurlan Muldashev <nurlan.muldashev@metaphor.kz>
2017-04-29 13:52:18 +0600

    Исправил ошибку при невыбранном файле

--- nginx/html/customScripts/job-request.js
+++ nginx/html/customScripts/job-request.js
@@ -150,6 +150,32 @@ define('kcell-custom-module', ['angular'], function (angular) {
       },
       template: '{{myModelExpression}} = {{myModelValue}}'  
     }
-  });
+  })
+  .directive('requiredFile',function(){
+    return {
+        require:'ngModel',
+        restrict: 'A',
+        link: function(scope,el,attrs,ctrl){
+
+        	function updateValidity(fileRequired){
+        		var valid = (fileRequired && el.val() !== '') || (!fileRequired);
+        		ctrl.$setValidity('validFile', valid);
+        		ctrl.$setValidity('camVariableType', valid);
+        	}
+
+        	updateValidity(scope.$eval(attrs.requiredFile));
+
+        	scope.$watch(attrs.requiredFile, updateValidity);
+
+            el.bind('change',function(){
+                updateValidity(scope.$eval(attrs.requiredFile));
+                scope.$apply(function(){
+                    ctrl.$setViewValue(el.val());
+                    ctrl.$render();
+                });
+            });
+        }
+    }
+});
   return customModule;
 });
