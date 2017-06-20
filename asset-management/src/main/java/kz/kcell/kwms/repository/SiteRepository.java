d7e150a930bd23a26778af2397f7255a7874d251
Nurlan Muldashev <nurlan.muldashev@metaphor.kz>
2017-06-20 16:03:24 +0600

    Выбор сайтов related to

--- asset-management/src/main/java/kz/kcell/kwms/repository/SiteRepository.java
+++ asset-management/src/main/java/kz/kcell/kwms/repository/SiteRepository.java
@@ -1,6 +1,8 @@
 package kz.kcell.kwms.repository;
 
 import kz.kcell.kwms.model.Site;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 
@@ -9,6 +11,6 @@ import java.util.List;
 public interface SiteRepository extends PagingAndSortingRepository<Site, Long> {
 
     List<Site> findByName(@Param("name") String name);
-    List<Site> findByNameIgnoreCaseContaining(@Param("name") String name);
+    Page<Site> findByNameIgnoreCaseContaining(@Param("name") String name, Pageable p);
 
 }
