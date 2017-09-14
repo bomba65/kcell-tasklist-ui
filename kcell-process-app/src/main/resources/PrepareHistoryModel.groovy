8fd8b4475106a24a96c456d63149cfc3737ffb6a
Maxat Zhuniskhanov <zhmaksat@gmail.com>
2017-09-14 14:27:28 +0600

    CreatePRNotification freemarker changed to groovy template

--- kcell-process-app/src/main/resources/PrepareHistoryModel.groovy
+++ kcell-process-app/src/main/resources/PrepareHistoryModel.groovy
@@ -4,22 +4,13 @@ import org.camunda.bpm.engine.delegate.DelegateExecution
 def getUserEmail(DelegateExecution execution) {
     def identityService = execution.processEngineServices.identityService
     def jsonSlurper = new JsonSlurperClassic()
-    println "resolutions_________"
-    println execution.getVariable("resolutions")
-    println execution.getVariable("resolutions").toString()
-
     def resolutions = jsonSlurper.parseText(execution.getVariable("resolutions").toString())
-    println "_________resolutions"
-    println resolutions
-    println "_________resolutions12313"
 
     def taskMap = [:]
     def assigneeMap = [:]
 
     resolutions.each {
-        println it
         def user = identityService.createUserQuery().userId(it["assignee"]).singleResult()
-        println user
         assigneeMap.put(user.id, (user.firstName != null ? user.firstName : "")+" " +(user.lastName !=null ? user.lastName : ""))
         taskMap.put(it.taskId, execution.processEngineServices.historyService.createHistoricTaskInstanceQuery().taskId(it.taskId).singleResult())
     }
@@ -27,6 +18,9 @@ def getUserEmail(DelegateExecution execution) {
     def historyModel = [:]
     historyModel.put("assigneesMap", assigneeMap)
     historyModel.put("tasksMap", taskMap)
+    println "_________before historyModel"
+    println historyModel
+    println "_________after historyModel"
     historyModel
 }
 
