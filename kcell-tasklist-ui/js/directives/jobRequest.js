6b5042da91ee90e6a08da879fb2364430fdbb431
Maxat Zhuniskhanov <zhmaksat@gmail.com>
2017-10-20 17:11:18 +0600

    https://pm.kcell.kz/browse/KWMS-349, Приватность прикрепляемых файлов и перевод хранения их на минио

--- kcell-tasklist-ui/js/directives/jobRequest.js
+++ kcell-tasklist-ui/js/directives/jobRequest.js
@@ -1,11 +1,30 @@
 define(['./module'], function(module){
 	'use strict';
-	module.directive('jobRequest', function () {
+	module.directive('jobRequest', function ($rootScope, $http) {
 		return {
 			restrict: 'E',
 			scope: {
-				jobModel: '='
+				jobModel: '=',
+				processInstanceId: '=',
+				taskId: '='
 			},
+			link: function(scope, element, attrs) {
+	            scope.download = function(file) {
+	                $http({method: 'GET', url: '/camunda/uploads/get/' + scope.processInstanceId + '/' + scope.taskId + '/' + file.value.name, transformResponse: [] }).
+	                success(function(data, status, headers, config) {
+	                    document.getElementById('fileDownloadIframe').src = data;
+	                }).
+	                error (function(data, status, headers, config) {
+	                    console.log(data);
+	                });
+               	};
+	            scope.isFileVisible = function(file) {
+	            	return !file.value.visibility || file.value.visibility == 'all' || (file.value.visibility == 'kcell' && $rootScope.hasGroup('kcellUsers'));
+	            }
+	            scope.isVisible = function(visibility) {
+	            	return !visibility || !visibility.value || visibility.value == 'all' ||  (visibility.value == 'kcell' && $rootScope.hasGroup('kcellUsers'));
+	            }
+	        },
 			templateUrl: './js/directives/jobRequest.html'
 		};
 	});
