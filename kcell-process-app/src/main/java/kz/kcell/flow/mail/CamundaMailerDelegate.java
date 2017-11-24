d6bbc69979622664e4e0532f577e60b08ff477a1
Maxat Zhuniskhanov <zhmaksat@gmail.com>
2017-11-24 15:57:12 +0600

    Проставление отправителя при отправке почты с CamundaMailerDelegate

--- kcell-process-app/src/main/java/kz/kcell/flow/mail/CamundaMailerDelegate.java
+++ kcell-process-app/src/main/java/kz/kcell/flow/mail/CamundaMailerDelegate.java
@@ -3,25 +3,30 @@ package kz.kcell.flow.mail;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import javax.mail.internet.MimeMessage;
 
 public class CamundaMailerDelegate implements JavaDelegate {
 
+    @Value("${mail.sender:flow@kcell.kz}")
+    private String sender;
+
     @Autowired
-    private JavaMailSender sender;
+    private JavaMailSender mailSender;
 
     @Override
     public void execute(DelegateExecution delegateExecution) throws Exception {
 
-        MimeMessage message = sender.createMimeMessage();
+        MimeMessage message = mailSender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(message);
         helper.setTo(separateEmails(String.valueOf(delegateExecution.getVariableLocal("to"))));
         helper.setText(String.valueOf(delegateExecution.getVariableLocal("html")), true);
         helper.setSubject(String.valueOf(delegateExecution.getVariableLocal("subject")));
+        helper.setFrom(sender);
 
-        sender.send(message);
+        mailSender.send(message);
     }
 
     private String[] separateEmails(String addresses){
