cd39c2b3724ed1e4468a2ddaf0b8b4b913b7a8ce
Maxat Zhuniskhanov <zhmaksat@gmail.com>
2017-12-06 17:01:39 +0600

    Invoice: Set status invoiced in Revision processes

---
+++ kcell-process-app/src/main/java/kz/kcell/bpm/SetInvoicingStatus.java
@@ -0,0 +1,36 @@
+package kz.kcell.bpm;
+
+import org.camunda.bpm.engine.RuntimeService;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.camunda.bpm.engine.runtime.Execution;
+import org.camunda.bpm.engine.runtime.ProcessInstance;
+import org.camunda.spin.SpinList;
+import org.camunda.spin.json.SpinJsonNode;
+import org.camunda.spin.plugin.variable.value.JsonValue;
+
+import java.util.logging.Logger;
+import java.util.stream.Stream;
+
+public class SetInvoicingStatus implements JavaDelegate {
+
+    private final static Logger LOGGER = Logger.getLogger(SetInvoicingStatus.class.getName());
+
+    @Override
+    public void execute(DelegateExecution delegateExecution) throws Exception {
+
+        RuntimeService runtimeService = delegateExecution.getProcessEngineServices().getRuntimeService();
+
+        SpinJsonNode selectedWorks = delegateExecution.<JsonValue>getVariableTyped("selectedWorks").getValue();
+        for(String field: selectedWorks.fieldNames()){
+            if(selectedWorks.prop(field).isArray()){
+                SpinList<SpinJsonNode> requests = selectedWorks.prop(field).elements();
+                requests.forEach(request -> {
+                    String revisionId = request.prop("processInstanceId").stringValue();
+                    runtimeService.setVariable(revisionId, "acceptPerformedJob", "invoiced");
+                });
+            }
+        }
+    }
+
+}
