fac6d8ab8934f36e943b98e2d7726d7acaa025a7
Maxat Zhuniskhanov <zhmaksat@gmail.com>
2018-02-14 10:49:48 +0600

    Invoice: Check dublicate process at start and modify task

--- kcell-process-app/src/main/java/kz/kcell/bpm/assignments/InvoiceRegionContractorAssignmentHandler.java
+++ kcell-process-app/src/main/java/kz/kcell/bpm/assignments/InvoiceRegionContractorAssignmentHandler.java
@@ -7,11 +7,13 @@ public class InvoiceRegionContractorAssignmentHandler implements TaskListener {
 
     @Override
     public void notify(DelegateTask delegateTask) {
-        StringBuilder group = new StringBuilder("_contractor_lse");
+        StringBuilder group = new StringBuilder("_contractor_");
 
         String region = delegateTask.getVariable("region").toString();
+        String contractor = delegateTask.getVariable("contractor").toString();
         if (region != null) {
             group.insert(0, region);
+            group.append(contractor);
             delegateTask.addCandidateGroup(group.toString());
         }
     }
