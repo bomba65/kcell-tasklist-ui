32752f9fb125a9f75c03fa23fd670c251ada01cc
Jamshed <h.jamik@gmail.com>
2018-06-28 01:33:24 +0600

    'Version' field as input

--- kcell-tasklist-ui/js/directives/demand/details.js
+++ kcell-tasklist-ui/js/directives/demand/details.js
@@ -10,6 +10,15 @@ define(['./../module'], function(module){
                 readonly: '='
 			},
 			link: function(scope, element, attrs) {
+				scope.multiselectSettings = {
+					enableSearch: true,
+					smartButtonMaxItems: 3,
+					showCheckAll: false,
+					showUncheckAll: false,
+					displayProp: 'v',
+					idProp: 'v',
+					externalIdProp: 'v'
+				};
 				scope.productNameOptions = [
 					{v: 'Мобильный круг'}
 				]
@@ -24,12 +33,31 @@ define(['./../module'], function(module){
 					{v: 'TV cmpaign'},
 					{v: 'Billboards'}
 				];
+
+				scope.productNameAdd = function() {
+					var val = prompt("Please enter product name");
+					if (val && val.length) {
+						scope.productNameOptions.push({v: val});
+					}
+				}
+
+				scope.offerNameAdd = function() {
+					var val = prompt("Please enter product offer name");
+					if (val && val.length) {
+						scope.offerNameOptions.push({v: val});
+					}
+				}
+
 				scope.$watch('data', function(value) {
 					if (value) {
 						if (!scope.data) scope.data = {};
 						if (!scope.data.productName) scope.data.productName = [];
+						else scope.productNameOptions = _.unionWith(scope.productNameOptions, scope.data.productName, _.isEqual);
 						if (!scope.data.productOfferName) scope.data.productOfferName = [];
+						else scope.offerNameOptions = _.unionWith(scope.offerNameOptions, scope.data.productOfferName, _.isEqual);
 						if (!scope.data.deliverable) scope.data.deliverable = [];
+						if (!scope.data.productNameVersion) scope.data.productNameVersion = '1.0';
+						if (!scope.data.offerNameVersion) scope.data.offerNameVersion = '1.0';
 					}
 				});
 	        },
