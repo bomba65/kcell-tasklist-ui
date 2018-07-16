839247b29837af90d74c94ca8027a65009f5fe64
Jamshed <h.jamik@gmail.com>
2018-07-17 03:22:40 +0600

    multiselect-dropdown changed to autocomplete in details

--- kcell-tasklist-ui/js/directives/demand/details.js
+++ kcell-tasklist-ui/js/directives/demand/details.js
@@ -10,6 +10,15 @@ define(['./../module'], function(module){
                 readonly: '='
 			},
 			link: function(scope, element, attrs) {
+				scope.$watch('data', function(value) {
+					if (value) {
+						if (!scope.data) scope.data = {};
+						if (!scope.data.productNames) scope.data.productNames = [];
+						if (!scope.data.offerNames) scope.data.offerNames = [];
+						if (!scope.data.deliverable) scope.data.deliverable = [];
+					}
+				});
+				
 				scope.multiselectSettings = {
 					enableSearch: true,
 					smartButtonMaxItems: 3,
@@ -19,12 +28,6 @@ define(['./../module'], function(module){
 					idProp: 'v',
 					externalIdProp: 'v'
 				};
-				scope.productNameOptions = [
-					{v: 'Мобильный круг'}
-				]
-				scope.offerNameOptions = [
-					{v: 'Мобильный круг S'}
-				]
 				scope.deliverableOptions = [
 					{v: 'Charging logic in BSS'},
 					{v: 'USSD channel'},
@@ -34,32 +37,64 @@ define(['./../module'], function(module){
 					{v: 'Billboards'}
 				];
 
+				// ------ Product name -----
+				var productNameOptions = [
+					{ name: 'Мобильный круг', version: 1.0 }
+				];
+				scope.filteredProductNames = [];
 				scope.productNameAdd = function() {
-					var val = prompt("Please enter product name");
-					if (val && val.length) {
-						scope.productNameOptions.push({v: val});
-					}
+					scope.data.productNames.push({ name: '', version: null });
+					scope.filteredProductNames.push([]);
+				}
+				scope.productNameDelete = function(index) {
+					scope.data.productNames.splice(index, 1);
+					scope.filteredProductNames.splice(index, 1);
+				}
+				scope.productNameComplete = function(index) {
+					scope.filteredProductNames[index] = _.filter(productNameOptions, function(opt) {
+						return opt.name.startsWith(scope.data.productNames[index].name);
+					});
+				}
+				scope.productNameSelect = function(index, selected) {
+					scope.data.productNames[index] = _.clone(selected);
+					scope.productNameBlur(index);
+				}
+				scope.productNameBlur = function(index) {
+					if (!scope.data.productNames[index].version)
+						scope.data.productNames[index].version = 1.0;
+					scope.filteredProductNames[index] = null;
 				}
 
+				// ------ Product offer name ------
+				var offerNameOptions = [
+					{name: 'Мобильный круг S', description:'', version: 1.0}
+				];
+				scope.filteredOfferNames = [];
 				scope.offerNameAdd = function() {
-					var val = prompt("Please enter product offer name");
-					if (val && val.length) {
-						scope.offerNameOptions.push({v: val});
-					}
+					scope.data.offerNames.push({ name: '', version: null });
+					scope.filteredOfferNames.push([]);
 				}
-
-				scope.$watch('data', function(value) {
-					if (value) {
-						if (!scope.data) scope.data = {};
-						if (!scope.data.productName) scope.data.productName = [];
-						else scope.productNameOptions = _.unionWith(scope.productNameOptions, scope.data.productName, _.isEqual);
-						if (!scope.data.productOfferName) scope.data.productOfferName = [];
-						else scope.offerNameOptions = _.unionWith(scope.offerNameOptions, scope.data.productOfferName, _.isEqual);
-						if (!scope.data.deliverable) scope.data.deliverable = [];
-						if (!scope.data.productNameVersion) scope.data.productNameVersion = '1.0';
-						if (!scope.data.offerNameVersion) scope.data.offerNameVersion = '1.0';
-					}
-				});
+				scope.offerNameDelete = function(index) {
+					scope.data.offerNames.splice(index, 1);
+					scope.filteredOfferNames.splice(index, 1);
+				}
+				scope.offerNameComplete = function(index) {
+					scope.filteredOfferNames[index] = _.filter(offerNameOptions, function(opt) {
+						return opt.name.startsWith(scope.data.offerNames[index].name);
+					});
+				}
+				scope.offerNameSelect = function(index, selected) {
+					scope.data.offerNames[index] = _.clone(selected);
+					scope.offerNameBlur(index);
+				}
+				scope.offerNameFocus = function(index) {
+					scope.offerNameComplete(index);
+				}
+				scope.offerNameBlur = function(index) {
+					if (!scope.data.offerNames[index].version)
+						scope.data.offerNames[index].version = 1.0;
+					scope.filteredOfferNames[index] = null;
+				}
 	        },
 			templateUrl: './js/directives/demand/details.html'
 		};
