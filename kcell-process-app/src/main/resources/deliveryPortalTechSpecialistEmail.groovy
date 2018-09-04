9e8d466a301dabde60354661778ec43a7a42f69f
Dauren Amirzhanov <dauren.amirzhanov@gmail.com>
2018-09-04 22:24:49 +0600

    DP-88: Добавил инициатора в email-рассылку login & pass

--- kcell-process-app/src/main/resources/deliveryPortalTechSpecialistEmail.groovy
+++ kcell-process-app/src/main/resources/deliveryPortalTechSpecialistEmail.groovy
@@ -1,8 +1,28 @@
 import org.camunda.bpm.engine.delegate.DelegateExecution
+import java.util.stream.Collectors
 
 def getTechSpecialistEmail(DelegateExecution execution) {
     def responsibleB2BSales = execution.getVariable("techSpecEmail").toString()
     println (responsibleB2BSales)
-    responsibleB2BSales
+    def starterEmails = getStarterEmails(execution)
+    def result = starterEmails.stream().collect(Collectors.joining(",")) + ', ' + responsibleB2BSales
+    println 'get users:'
+    println result
+    result
+}
+
+def getStarterEmails(DelegateExecution execution) {
+    def identityService = execution.processEngineServices.identityService
+
+    identityService.createUserQuery().userId(starter).list().stream()
+            .map{it.email}
+            .filter{it != null && !it.empty}
+            .collect(Collectors.toSet())
 }
 getTechSpecialistEmail(execution)
+
+
+
+
+
+
