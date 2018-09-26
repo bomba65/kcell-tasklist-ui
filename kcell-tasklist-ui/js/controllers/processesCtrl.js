4ff251de562322fe07909e309708168585e0cd19
Maxat Zhuniskhanov <zhmaksat@gmail.com>
2018-09-26 11:17:08 +0600

    Tooltips fixed and general info about work request type added

--- kcell-tasklist-ui/js/controllers/processesCtrl.js
+++ kcell-tasklist-ui/js/controllers/processesCtrl.js
@@ -50,7 +50,7 @@ define(['./module','jquery', 'camundaSDK'], function(app, $, CamSDK){
 
 		var catalogs = {};
 
-        $http.get('/api/catalogs?force=true').then(
+        $http.get('/api/catalogs?force=1').then(
             function (result) {
                 angular.extend(catalogs, result.data);
             },
