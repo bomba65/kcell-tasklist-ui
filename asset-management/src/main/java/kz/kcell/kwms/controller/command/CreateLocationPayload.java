519d38d217d1f5089a26a1ee2a07e128f08265f4
Maxat Zhuniskhanov <zhmaksat@gmail.com>
2018-10-01 23:27:54 +0600

    Sloc and site relation by sitename

--- asset-management/src/main/java/kz/kcell/kwms/controller/command/CreateLocationPayload.java
+++ asset-management/src/main/java/kz/kcell/kwms/controller/command/CreateLocationPayload.java
@@ -8,15 +8,10 @@ import java.util.Map;
 public class CreateLocationPayload implements Payload {
 
     public String name;
-    public Long siteId;
+    public String sitename;
     public JsonNode params;
 
     @Override
     public <T extends Payload> void execute(Command tCommand, CommandController commandController, Map<String, Object> objectMap) {
-/*        commandController.locationRepository.save(Location.builder()
-                .name(this.name)
-                .site(commandController.siteRepository.findOne(this.siteId))
-                .params(this.params != null ? this.params.toString() : "{}")
-                .build());
-*/    }
+    }
 }
