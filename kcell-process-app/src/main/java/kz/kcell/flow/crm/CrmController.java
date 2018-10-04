6d97a84e68766eb848243b877f0e5b74ff7f2fbf
Daniyar Kurenov <kurenov@gmail.com>
2018-10-04 16:12:52 +0600

    DP-90 add check for customer existance by accountName

--- kcell-process-app/src/main/java/kz/kcell/flow/crm/CrmController.java
+++ kcell-process-app/src/main/java/kz/kcell/flow/crm/CrmController.java
@@ -148,9 +148,9 @@ public class CrmController {
                 regionalHeadUser.put("lastName", "Шевелева");
                 regionalHeadUser.put("middleName", "Николаевна");
                 fakeContent.put("regionalHeadUser", regionalHeadUser);
-            }
 
-            if (clientBIN.equals("111222333444")) {
+            } else if (clientBIN.equals("111222333444")) {
+
                 fakeContent.put("id", 80607);
                 fakeContent.put("accountName", "Тестовый Клиент");
                 fakeContent.put("accountTyp", "Prospect");
@@ -209,6 +209,9 @@ public class CrmController {
                 regionalHeadUser.put("lastName", "-");
                 //regionalHeadUser.put("middleName", null);
                 fakeContent.put("regionalHeadUser", regionalHeadUser);
+            } else {
+                //return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("{\"timestamp\":\"2018-10-04T15:19:17.655\",\"status\":500,\"code\":\"ERR0007\",\"subCode\":\"INTERNAL_ERROR\",\"message\":\"Corp client profile with bin 888999888999 doesn't exist\",\"path\":\"/corp_client_profile/bin/888999888999\",\"requestId\":\"20181004_151917_537_N9o\"}");
+                return ResponseEntity.ok("{\"timestamp\":\"2018-10-04T15:19:17.655\",\"status\":500,\"code\":\"ERR0007\",\"subCode\":\"INTERNAL_ERROR\",\"message\":\"Corp client profile with bin 888999888999 doesn't exist\",\"path\":\"/corp_client_profile/bin/888999888999\",\"requestId\":\"20181004_151917_537_N9o\"}");
             }
 
             return ResponseEntity.ok(fakeContent.toString());
