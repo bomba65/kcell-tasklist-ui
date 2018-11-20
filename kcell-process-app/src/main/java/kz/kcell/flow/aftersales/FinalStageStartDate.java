10876c35f69220275ef5f3ff95e569ba7c16fbb7
Jamshed Haitov <h.jamik@gmail.com>
2018-11-20 21:37:27 +0600

    ready for testing

---
+++ kcell-process-app/src/main/java/kz/kcell/flow/aftersales/FinalStageStartDate.java
@@ -0,0 +1,68 @@
+package kz.kcell.flow.aftersales;
+
+import org.aopalliance.intercept.MethodInvocation;
+import org.apache.commons.lang.StringUtils;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClients;
+import org.camunda.bpm.engine.HistoryService;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.camunda.bpm.engine.history.HistoricProcessInstance;
+import org.camunda.spin.impl.json.jackson.JacksonJsonNode;
+import org.msgpack.util.json.JSON;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
+import org.apache.http.ssl.SSLContextBuilder;
+import org.apache.http.impl.client.CloseableHttpClient;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.util.List;
+import java.util.Map;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
+
+@Service("FinalStageStartDate")
+@lombok.extern.java.Log
+
+public class FinalStageStartDate implements JavaDelegate {
+
+    @Override
+    public void execute(DelegateExecution delegateExecution) throws Exception {
+
+        Calendar c = Calendar.getInstance();
+        c.setTime(new Date());
+        c.set(Calendar.HOUR_OF_DAY, 9);
+        c.set(Calendar.MINUTE, 0);
+        c.set(Calendar.SECOND, 0);
+        c.set(Calendar.MILLISECOND, 0);
+        int dayOfWeek = c.get(Calendar.DAY_OF_WEEK);
+        int dayOfMonth = c.get(Calendar.DAY_OF_MONTH);
+
+        if (dayOfWeek > 2 && dayOfWeek < 6) {
+            c.set(Calendar.DAY_OF_MONTH, dayOfMonth + 5 - dayOfWeek);
+        } else if (dayOfWeek < 3) {
+            c.set(Calendar.DAY_OF_MONTH, dayOfMonth + 2 - dayOfWeek);
+        } else {
+            c.set(Calendar.DAY_OF_MONTH, dayOfMonth + 9 - dayOfWeek);
+        }
+
+        DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+        formatter.setTimeZone(TimeZone.getTimeZone("UTC"));
+        String startDate = formatter.format(c.getTime());
+
+        delegateExecution.setVariable("startDate", startDate);
+    }
+
+
+}
