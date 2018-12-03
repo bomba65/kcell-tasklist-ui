38a9e4ab745d3d25533200ee1368e152837dd001
Maxat Zhuniskhanov <zhmaksat@gmail.com>
2018-12-03 22:28:37 +0600

    Leasing: RR file generate variables send check

--- kcell-process-app/src/main/java/kz/kcell/flow/leasing/LeasingController.java
+++ kcell-process-app/src/main/java/kz/kcell/flow/leasing/LeasingController.java
@@ -1,5 +1,6 @@
 package kz.kcell.flow.leasing;
 
+import kz.kcell.flow.files.Minio;
 import lombok.extern.java.Log;
 import org.camunda.bpm.engine.IdentityService;
 import org.camunda.bpm.engine.RuntimeService;
@@ -11,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.script.*;
+import java.io.ByteArrayInputStream;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 
@@ -27,18 +29,21 @@ public class LeasingController {
     @Autowired
     RuntimeService runtimeService;
 
+    private Minio minioClient;
 
     @Autowired
-    public LeasingController(ScriptEngineManager manager) throws ScriptException {
+    public LeasingController(ScriptEngineManager manager, Minio minioClient) throws ScriptException {
         ScriptEngine groovy = manager.getEngineByName("groovy");
         InputStreamReader reader = new InputStreamReader(LeasingController.class.getResourceAsStream("/template/leasing/rent_req.groovy"));
         this.template = ((Compilable)groovy).compile(reader);
+
+        this.minioClient = minioClient;
     }
 
 
     @RequestMapping(value = "/rrfile/create", method = RequestMethod.POST, produces={"application/json"}, consumes="application/json")
     @ResponseBody
-    public ResponseEntity<String> FreephoneClientCreateUpdate(@RequestBody String saoRequestBody) throws Exception {
+    public ResponseEntity<String> FreephoneClientCreateUpdate(@RequestBody RRFileData rrFileData) throws Exception {
 
         if (identityService.getCurrentAuthentication() == null || identityService.getCurrentAuthentication().getUserId() == null) {
             log.warning("No user logged in");
@@ -47,11 +52,14 @@ public class LeasingController {
 
         try {
             Bindings bindings = template.getEngine().createBindings();
-//            bindings.put("delegateTask", delegateTask);
-            String actOfAcceptanceHtml = String.valueOf(template.eval(bindings));
 
-            FileValue actOfAcceptanceDocument = null;
-            actOfAcceptanceDocument = Variables.fileValue("actOfAcceptance.doc").file(actOfAcceptanceHtml.getBytes("utf-8")).mimeType("application/msword").create();
+            String rrFileHtml = String.valueOf(template.eval(bindings));
+            ByteArrayInputStream is = new ByteArrayInputStream(rrFileHtml.getBytes("utf-8"));
+
+            minioClient.saveFile(rrFileData.processInstanceId + "/" + rrFileData.taskId + "/rrFile.docx" , is, "application/vnd.openxmlformats-officedocument.wordprocessingml.document");
+
+            System.out.println(rrFileData.processInstanceId + "/" + rrFileData.taskId + "/rrFile.docx");
+
         } catch (ScriptException e) {
             throw new RuntimeException(e.getMessage());
         } catch (UnsupportedEncodingException e) {
---
+++ kcell-process-app/src/main/java/kz/kcell/flow/leasing/RRFileData.java
@@ -0,0 +1,28 @@
+package kz.kcell.flow.leasing;
+
+public class RRFileData {
+
+    public String processInstanceId;
+    public String taskId;
+    public String ncpID;
+    public String siteName;
+    public String dateOfVisit;
+    public String cellAntennaAddress;
+    public String contactName;
+    public String contactPosition;
+    public String contactInfo;
+    public String lattitude;
+    public String longitude;
+    public String constructionType;
+    public String square;
+    public String rbsType;
+    public String selectedBands;
+    public String selectedCellAntennas;
+    public String cellAntennaQuantity;
+    public String cellAntennaDimensions;
+    public String cellAntennaWeight;
+    public String cellAntennaSuspensionHeight;
+    public String cellAntennaAzimuth;
+//    newCandidateComments
+
+}
