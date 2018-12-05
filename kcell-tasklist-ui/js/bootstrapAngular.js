222bd5459f2c8bb522b7d8d33f3ffab86a4be848
Daniyar Kurenov <kurenov@gmail.com>
2018-12-05 14:57:53 +0600

    Fix check for Chrome browser

--- kcell-tasklist-ui/js/bootstrapAngular.js
+++ kcell-tasklist-ui/js/bootstrapAngular.js
@@ -3,7 +3,7 @@
 var isOpera = (!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
 
 // Chrome 1+
-var isChrome = !!window.chrome && !!window.chrome.webstore;
+var isChrome = !!window.chrome; //&& !!window.chrome.webstore;
 
 // Blink engine detection
 var isBlink = (isChrome || isOpera) && !!window.CSS;
