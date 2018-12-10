83152213df00e05bda762763ead53164f443f204
Daniyar Kurenov <kurenov@gmail.com>
2018-12-10 14:49:38 +0600

    DP-147 - change request content type

--- kcell-process-app/src/main/java/kz/kcell/flow/sao/FreephoneClientCreateUpdate.java
+++ kcell-process-app/src/main/java/kz/kcell/flow/sao/FreephoneClientCreateUpdate.java
@@ -3,7 +3,10 @@ package kz.kcell.flow.sao;
 import lombok.extern.java.Log;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
@@ -12,6 +15,7 @@ import org.apache.http.entity.ContentType;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.ssl.SSLContextBuilder;
 import org.apache.http.util.EntityUtils;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
@@ -31,7 +35,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.net.URI;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 
 @Service("freephoneClientSAO")
@@ -72,6 +78,7 @@ public class FreephoneClientCreateUpdate implements JavaDelegate {
         JSONObject requestBodyJSON = new JSONObject();
 
         /*
+        /////////////////////////////////////////////////////
         requestBodyJSON.put("fk_client", 0);
         requestBodyJSON.put("client_name", String.valueOf(delegateExecution.getVariable("officialClientCompanyName")));
         requestBodyJSON.put("bin", String.valueOf(delegateExecution.getVariable("clientBIN")));
@@ -102,7 +109,10 @@ public class FreephoneClientCreateUpdate implements JavaDelegate {
             requestBodyJSON.put("rtdma", String.valueOf(delegateExecution.getVariable("RTDMA")));
             requestBodyJSON.put("klm", String.valueOf(delegateExecution.getVariable("channelKLM")));
         }
+        //////////////////////////////////////////////////////////
         */
+
+        /*
         requestBodyJSON.put("FK_CLIENT", 0);
         requestBodyJSON.put("CLIENT_NAME", String.valueOf(delegateExecution.getVariable("officialClientCompanyName")));
         requestBodyJSON.put("BIN", String.valueOf(delegateExecution.getVariable("clientBIN")));
@@ -133,9 +143,9 @@ public class FreephoneClientCreateUpdate implements JavaDelegate {
             requestBodyJSON.put("RTDMA", String.valueOf(delegateExecution.getVariable("RTDMA")));
             requestBodyJSON.put("KLM", String.valueOf(delegateExecution.getVariable("channelKLM")));
         }
-
         if (isSftp) {
             System.out.println("requestBodyJSON: " + requestBodyJSON.toString() + ", operators: " + operatorJSONArray.toString());
+
             for(int n = 0; n < operatorJSONArray.length(); n++) {
                 JSONObject operatorJSON = operatorJSONArray.getJSONObject(n);
 
@@ -177,9 +187,74 @@ public class FreephoneClientCreateUpdate implements JavaDelegate {
             System.out.println("saoRequest: " + saoRequest.toString());
             System.out.println("saoResponse: " + saoResponse.toString());
             System.out.println("saoSuccessfulResponse: " + saoSuccessfulResponse);
+        */
+        if (isSftp) {
+            System.out.println("requestBodyJSON: " + requestBodyJSON.toString() + ", operators: " + operatorJSONArray.toString());
 
-            // cast variables to number according to number type fields in confirmLAstMileFinishConstruction.html, fillConnectionInformation.html, createApplicationToConnection.html
-            // add specific data to requestBodyJSON according to DP-147
+            for(int n = 0; n < operatorJSONArray.length(); n++) {
+
+                JSONObject operatorJSON = operatorJSONArray.getJSONObject(n);
+
+                List<NameValuePair> params = new ArrayList<NameValuePair>();
+
+                params.add(new BasicNameValuePair("report", operatorJSON.get("name").toString()));
+                params.add(new BasicNameValuePair("fk_client", "0"));
+                params.add(new BasicNameValuePair("client_name", String.valueOf(delegateExecution.getVariable("officialClientCompanyName"))));
+                params.add(new BasicNameValuePair("bin", String.valueOf(delegateExecution.getVariable("clientBIN"))));
+                params.add(new BasicNameValuePair("technology", "IVR"));
+                params.add(new BasicNameValuePair("short_number", title));
+                params.add(new BasicNameValuePair("type_conn", connectionType));
+
+                if("onnet".equals(operatorType)){
+                    params.add(new BasicNameValuePair("provider", "Kcell"));
+                } else {
+                    params.add(new BasicNameValuePair("provider", String.valueOf(delegateExecution.getVariable("provider"))));
+                }
+
+                if("SIP_SBC".equals(connectionType)){
+                    params.add(new BasicNameValuePair("ip", String.valueOf(delegateExecution.getVariable("ipNumber"))));
+                } else if("transmit".equals(connectionType)){
+                    params.add(new BasicNameValuePair("forward_num", String.valueOf(delegateExecution.getVariable("transmitNumber"))));
+                } else if("E1".equals(connectionType)){
+                    params.add(new BasicNameValuePair("term_point", String.valueOf(delegateExecution.getVariable("terminationPoint"))));
+                    params.add(new BasicNameValuePair("hope_point", String.valueOf(delegateExecution.getVariable("hopPoint"))));
+                    params.add(new BasicNameValuePair("site_name", String.valueOf(delegateExecution.getVariable("siteName"))));
+                    params.add(new BasicNameValuePair("port", String.valueOf(delegateExecution.getVariable("lastMilePort"))));
+                    params.add(new BasicNameValuePair("exchange", String.valueOf(delegateExecution.getVariable("EXCHANGE"))));
+                    params.add(new BasicNameValuePair("mgw", String.valueOf(delegateExecution.getVariable("MGW"))));
+                    params.add(new BasicNameValuePair("rtdma", String.valueOf(delegateExecution.getVariable("RTDMA"))));
+                    params.add(new BasicNameValuePair("klm", String.valueOf(delegateExecution.getVariable("channelKLM"))));
+                }
+                saoRequest.put(operatorJSON.get("name").toString(), params.toString());
+
+                CloseableHttpClient client = HttpClients.createDefault();
+                HttpPost httpPost = new HttpPost(new URI(baseUri+"/FreephoneClientCreateUpdate"));
+
+                httpPost.setEntity(new UrlEncodedFormEntity(params));
+                CloseableHttpResponse response = client.execute(httpPost);
+                HttpEntity entity = response.getEntity();
+                String responseString = EntityUtils.toString(entity, "UTF-8");
+
+                JSONObject responseJSON = new JSONObject(responseString);
+                saoResponse.put(operatorJSON.get("name").toString(), responseJSON);
+                if(responseJSON.has("success")) {
+                    Integer responseStatusCode = (Integer) responseJSON.get("success");
+                    if(responseStatusCode != 1) {
+                        saoSuccessfulResponse = false;
+                    }
+                } else {
+                    saoSuccessfulResponse = false;
+                }
+
+                client.close();
+            }
+            JsonValue saoRequestJsonValue = SpinValues.jsonValue(saoRequest.toString()).create();
+            delegateExecution.setVariable("saoRequest", saoRequestJsonValue);
+
+            JsonValue saoResponseJsonValue = SpinValues.jsonValue(saoResponse.toString()).create();
+            delegateExecution.setVariable("saoResponse", saoResponseJsonValue);
+
+            delegateExecution.setVariable("saoSuccessfulResponse", saoSuccessfulResponse);
 
         } else {
             System.out.println("requestBodyJSON: " + requestBodyJSON.toString() + ", operators: " + operatorJSONArray.toString());
