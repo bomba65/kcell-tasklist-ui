d961f8feadaf93ebc9f8dd13f0a9d299f79ffcfb
Maxat Zhuniskhanov <zhmaksat@gmail.com>
2019-01-25 11:36:36 +0600

    Camunda upgrade from 7.9 на 7.10

--- kcell-process-app/src/main/java/kz/kcell/flow/CamundaApplication.java
+++ kcell-process-app/src/main/java/kz/kcell/flow/CamundaApplication.java
@@ -21,7 +21,6 @@ import org.camunda.bpm.engine.impl.cfg.ProcessEnginePlugin;
 import org.camunda.bpm.engine.rest.mapper.JacksonConfigurator;
 import org.camunda.bpm.engine.spring.SpringProcessEngineConfiguration;
 import org.camunda.bpm.spring.boot.starter.SpringBootProcessApplication;
-import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
 import org.camunda.bpm.spring.boot.starter.util.SpringBootProcessEnginePlugin;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
