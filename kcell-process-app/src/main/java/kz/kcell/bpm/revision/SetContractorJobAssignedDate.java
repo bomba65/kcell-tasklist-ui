21a8a3e7f06cfe9d892b28338456cc41d48ee09a
Maxat Zhuniskhanov <zhmaksat@gmail.com>
2019-01-26 21:37:48 +0600

    Revision: KWMS-640, job request date setting logic changed

---
+++ kcell-process-app/src/main/java/kz/kcell/bpm/revision/SetContractorJobAssignedDate.java
@@ -0,0 +1,31 @@
+package kz.kcell.bpm.revision;
+
+import org.camunda.bpm.engine.delegate.DelegateTask;
+import org.camunda.bpm.engine.delegate.TaskListener;
+
+import java.util.Date;
+
+public class SetContractorJobAssignedDate implements TaskListener {
+
+    @Override
+    public void notify(DelegateTask delegateTask) {
+
+        if(delegateTask.hasVariable("contractorJobAssignedDate")){
+            Date contractorJobAssignedDate = (Date) delegateTask.getVariable("contractorJobAssignedDate");
+
+            long afterModifyList = delegateTask.getProcessEngineServices()
+                .getHistoryService()
+                .createHistoricTaskInstanceQuery()
+                .processInstanceId(delegateTask.getProcessInstanceId())
+                .taskDefinitionKey("modify_jr")
+                .finishedAfter(contractorJobAssignedDate)
+                .count();
+
+            if(afterModifyList > 0){
+                delegateTask.setVariable("contractorJobAssignedDate", new Date());
+            }
+        } else {
+            delegateTask.setVariable("contractorJobAssignedDate", new Date());
+        }
+    }
+}
