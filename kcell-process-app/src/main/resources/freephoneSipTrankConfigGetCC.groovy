c547b1b0a41eab92b183f080342c2556c4697757
dhcppc0 <smussakhojayeva@gmail.com>
2019-07-02 00:38:54 +0900

    DP-241 bugfix: starter to assignee email

--- kcell-process-app/src/main/resources/freephoneSipTrankConfigGetCC.groovy
+++ kcell-process-app/src/main/resources/freephoneSipTrankConfigGetCC.groovy
@@ -5,24 +5,19 @@ import java.util.stream.Collectors
 def getCcEmails(DelegateExecution execution) {
     def responsibleB2BDelivery = execution.getVariable("responsibleB2BDelivery").toString()
     def responsibleB2BDeliveryParse = new JsonSlurper().parseText(responsibleB2BDelivery)
-    def engineerEmail = getStarterEmails(execution).stream().collect(Collectors.joining(","))
+    def engineerEmail = getStarterEmails(execution)
     def result = engineerEmail + ', ' + responsibleB2BDeliveryParse.email
     execution.setVariable("mailOne", (engineerEmail.toString()))
     execution.setVariable("mailTwo", (responsibleB2BDeliveryParse.email.toString()))
-    def test = execution.getVariable('mailOne')
-
-
-    println 'get cc users:'
-    println result
     result
 }
 
 def getStarterEmails(DelegateExecution execution) {
+    def assignee = execution.processEngineServices
+            .getTaskService().createTaskQuery()
+            .processInstanceId(execution.getProcessInstanceId()).singleResult().getAssignee()
     def identityService = execution.processEngineServices.identityService
-
-    identityService.createUserQuery().userId(starter).list().stream()
-            .map{it.email}
-            .filter{it != null && !it.empty}
-            .collect(Collectors.toSet())
+    def user = identityService.createUserQuery().userId(assignee).singleResult()
+    user.getEmail()
 }
 getCcEmails(execution)
