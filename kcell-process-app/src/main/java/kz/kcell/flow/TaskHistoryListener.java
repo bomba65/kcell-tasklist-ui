ba9e0b679269e18aa450eb025b1b03d12dc0d58e
Jamshed Haitov <h.jamik@gmail.com>
2019-07-29 17:16:43 +0600

    PBX: Task notifications are enabled

--- kcell-process-app/src/main/java/kz/kcell/flow/TaskHistoryListener.java
+++ kcell-process-app/src/main/java/kz/kcell/flow/TaskHistoryListener.java
@@ -44,9 +44,7 @@ public class TaskHistoryListener implements TaskListener {
     @Autowired
     IdentityService identityService;
 
-    private final List<String> enabledProcesses = Arrays.asList("leasing", "freephone", "bulksmsConnectionKAE", "AftersalesPBX", "revolvingNumbers");
-
-    private final List<String> extraFieldsProcesses = Arrays.asList("AftersalesPBX", "revolvingNumbers");
+    private final List<String> enabledProcesses = Arrays.asList("leasing", "freephone", "bulksmsConnectionKAE", "AftersalesPBX", "revolvingNumbers", "PBX");
 
     @Override
     public void notify(DelegateTask delegateTask) {
