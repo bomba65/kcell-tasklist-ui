243c98bff145f5aeaad327c7a0367adc789e757c
Jamshed Haitov <h.jamik@gmail.com>
2019-08-13 16:40:28 +0600

    Headless Chrome added to generate PDF files from HTML-code

---
+++ headless-chrome/index.js
@@ -0,0 +1,68 @@
+const http = require('http');
+const puppeteer = require('puppeteer');
+
+const PORT = 8800;
+
+async function main() {
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
+  });
+  const page = await browser.newPage();
+
+  const requestHandler = async (request, response) => {
+    if (request.url === '/topdf' && request.method === 'POST') {
+      let body = '';
+      request.on('data', (data) => {
+        body += data;
+      });
+      request.on('end', async () => {
+        try {
+          await page.setContent(body);
+
+          const buffer = await page.pdf({
+            printBackground: true,
+            format: 'a4',
+            PreferCSSPageSize: true,
+            margin: {
+              top: '20px',
+              bottom: '20px'
+            }
+          });
+
+          response.setHeader("Content-Type", "application/pdf");
+          response.end(buffer);
+        } catch (e) {
+          response.writeHead(500, 'Internal Server Error');
+          response.end(e);
+        }
+      });
+    } else {
+      response.writeHead(404, 'Not found');
+      response.end();
+    }
+  };
+
+  const server = http.createServer(requestHandler);
+
+  server.listen(PORT, (err) => {
+    if (err) {
+      return console.log('Something went wrong: ', err)
+    }
+
+    console.log(`server is listening on ${PORT}`)
+  });
+
+  server.addListener('close', async () => {
+    if (page) await page.close();
+    if (browser) await browser.close();
+  });
+}
+
+(async () => {
+  try {
+    await main();
+  } catch (e) {
+    return console.log('Something went wrong: ', e)
+  }
+})();
