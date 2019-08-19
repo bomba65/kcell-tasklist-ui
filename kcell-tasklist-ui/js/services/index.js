48ee53f89305e87aed312d9851822222669047d6
Jamshed Haitov <h.jamik@gmail.com>
2019-08-19 10:59:07 +0600

    History variables manager service added

--- kcell-tasklist-ui/js/services/index.js
+++ kcell-tasklist-ui/js/services/index.js
@@ -5,5 +5,6 @@ define([
 	'./startProcessService',
 	'./searchCurrentSelectedProcessService',
 	'./disconnectSelectedProcessService',
-	'./htmlToPdf'
+	'./htmlToPdf',
+	'./historyVariablesManager'
 	], function(){});
