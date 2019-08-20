18b51581ece819701c75bba0e36ff5cc7a2a1537
Jamshed Haitov <h.jamik@gmail.com>
2019-08-20 23:21:26 +0600

    PBX: The process added to backend history listener

--- camundadb/docker-entrypoint-initdb.d/19_leasing.sql
+++ camundadb/docker-entrypoint-initdb.d/19_leasing.sql
@@ -7,11 +7,11 @@ INSERT INTO act_id_group (id_, rev_, name_, type_) VALUES ('LeasingRegionalGroup
 INSERT INTO act_id_group (id_, rev_, name_, type_) VALUES ('LeasingCentralGroupHeadSpecialist', 1, 'LeasingCentralGroupHeadSpecialist', 'WORKFLOW') ON CONFLICT(id_) DO NOTHING;
 INSERT INTO act_id_group (id_, rev_, name_, type_) VALUES ('LeasingRegionalPowerEngineer', 1, 'LeasingRegionalPowerEngineer', 'WORKFLOW') ON CONFLICT(id_) DO NOTHING;
 INSERT INTO act_id_group (id_, rev_, name_, type_) VALUES ('LeasingRegionalTransmissionGroup', 1, 'LeasingRegionalTransmissionGroup', 'WORKFLOW') ON CONFLICT(id_) DO NOTHING;
-INSERT INTO act_id_group (id_, rev_, name_, type_) VALUES ('LeasingPlanningGroup', 1, 'LeasingPlanningGroup', 'WORKFLOW') ON CONFLICT(id_) DO NOTHING
+INSERT INTO act_id_group (id_, rev_, name_, type_) VALUES ('LeasingPlanningGroup', 1, 'LeasingPlanningGroup', 'WORKFLOW') ON CONFLICT(id_) DO NOTHING;
 INSERT INTO act_id_group (id_, rev_, name_, type_) VALUES ('LeasingRegionalGroup', 1, 'LeasingRegionalGroup', 'WORKFLOW') ON CONFLICT(id_) DO NOTHING;
-INSERT INTO act_id_group (id_, rev_, name_, type_) VALUES ('LeasingCentralGroup', 1, 'LeasingCentralGroup', 'WORKFLOW') ON CONFLICT(id_) DO NOTHING
+INSERT INTO act_id_group (id_, rev_, name_, type_) VALUES ('LeasingCentralGroup', 1, 'LeasingCentralGroup', 'WORKFLOW') ON CONFLICT(id_) DO NOTHING;
 INSERT INTO act_id_group (id_, rev_, name_, type_) VALUES ('LeasingPowerEngineer', 1, 'LeasingPowerEngineer', 'WORKFLOW') ON CONFLICT(id_) DO NOTHING;
-INSERT INTO act_id_group (id_, rev_, name_, type_) VALUES ('LeasingInstallationGroup', 1, 'LeasingInstallationGroup', 'WORKFLOW') ON CONFLICT(id_) DO NOTHING
-INSERT INTO act_id_group (id_, rev_, name_, type_) VALUES ('LeasingRegionalPlanner', 1, 'LeasingRegionalPlanner', 'WORKFLOW') ON CONFLICT(id_) DO NOTHING
+INSERT INTO act_id_group (id_, rev_, name_, type_) VALUES ('LeasingInstallationGroup', 1, 'LeasingInstallationGroup', 'WORKFLOW') ON CONFLICT(id_) DO NOTHING;
+INSERT INTO act_id_group (id_, rev_, name_, type_) VALUES ('LeasingRegionalPlanner', 1, 'LeasingRegionalPlanner', 'WORKFLOW') ON CONFLICT(id_) DO NOTHING;
 
 commit;
