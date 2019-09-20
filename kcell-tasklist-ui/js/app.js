392e4011b673317a18cbd810672a269f3900d7b6
Maxat Zhuniskhanov <zhmaksat@gmail.com>
2019-09-20 17:03:50 +0600

    Dismantle and Replacement: added transmission types and alternatives is not required anymore

--- kcell-tasklist-ui/js/app.js
+++ kcell-tasklist-ui/js/app.js
@@ -316,7 +316,7 @@ define('app',[
 			}
 		}
 		$rootScope.getCatalogsHttpByName = function(name){
-			return '/api/' + name + '?v=3';
+			return '/api/' + name + '?v=4';
 		}
 	}]).run([ '$rootScope', '$location', 'AuthenticationService', '$q', '$state', function($rootScope, $location, AuthenticationService, $q, $state) {
 		$rootScope.$on('authentication.login.required', function(event) {
