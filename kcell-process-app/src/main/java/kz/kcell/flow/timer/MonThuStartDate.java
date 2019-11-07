4a0e069efedaa398fd5c33579ef9f81b73ccafed
Nurlan Muldashev <nurlan@muldashev.kz>
2019-11-08 00:01:04 +0600

    Timer Mon Thu

--- kcell-process-app/src/main/java/kz/kcell/flow/timer/MonThuStartDate.java
+++ kcell-process-app/src/main/java/kz/kcell/flow/timer/MonThuStartDate.java
@@ -10,7 +10,7 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.TimeZone;
 
-@Service("MonThuStartDate")
+@Service("monThuStartDate")
 public class MonThuStartDate implements JavaDelegate {
 
     @Override
