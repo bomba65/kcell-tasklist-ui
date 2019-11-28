a4323a9faa85aa985d545ad157b5c632c71743e7
Nurlan Muldashev <nurlan@muldashev.kz>
2019-11-28 23:19:51 +0600

    DP-345: Company Registration Date

--- kcell-tasklist-ui/js/directives/revolvingNumbers/legalInfo.js
+++ kcell-tasklist-ui/js/directives/revolvingNumbers/legalInfo.js
@@ -13,8 +13,6 @@ define(['./../module'], function(module){
       link: function(scope, element, attrs) {
         scope.$watch('data', function (value) {
           if (value) {
-            if (!scope.data.companyDate) scope.data.companyDate = new Date();
-            else scope.data.companyDate = new Date(scope.data.companyDate);
             if (!scope.data.termContract) scope.data.termContract = new Date();
             else scope.data.termContract = new Date(scope.data.termContract);
             if (!scope.data.clientPriority) scope.data.clientPriority = 'Normal';
