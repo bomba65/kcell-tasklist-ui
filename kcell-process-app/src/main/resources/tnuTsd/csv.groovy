d398e8cda354fecb763f68b21023a4230d591c9f
Maxat Zhuniskhanov <zhmaksat@gmail.com>
2020-04-29 12:49:35 +0600

    KWMS-922: fixed TowerHeight1 and TowerHeight2

--- kcell-process-app/src/main/resources/tnuTsd/csv.groovy
+++ kcell-process-app/src/main/resources/tnuTsd/csv.groovy
@@ -28,11 +28,11 @@ def binding = ["tnuTsdNumber":tnuTsdNumber,"ne_longitude":ne_longitude.replace("
                "ne_latitude":ne_latitude.replace(".",","),"fe_latitude":fe_latitude.replace(".",","),
                "ne_altitude":ne_altitude.replace(".",","),"fe_altitude":fe_altitude.replace(".",","),
                "ne_sitename":ne_sitename,"fe_sitename":fe_sitename,"ne_azimuth":ne_azimuth.replace(".",","),"fe_azimuth":fe_azimuth.replace(".",","),
-               "ne_construction_height":ne_construction_height.replace(".",","), "fe_construction_height":fe_construction_height.replace(".",",")]
+               "ne_suspension_height_antennas":ne_suspension_height_antennas.replace(".",","), "fe_suspension_height":fe_suspension_height.replace(".",",")]
 
 def template = ''' yieldUnescaped '"Name";"Name2";"Longitude";"Longitude2";"Latitude";"Latitude2";"Elevation";"Elevation2";"Site A";"Site B";"Azimuth1";"Azimuth2";"TowerHeight1";"TowerHeight2";"TXPower";"RadioProfile";"Tilt";"Feeder";"Parity";"Antenna_profile"'
 newLine()
-yieldUnescaped '"' + tnuTsdNumber + '";"' + tnuTsdNumber + '";"' + ne_longitude + '";"' + fe_longitude + '";"' + ne_latitude + '";"' + fe_latitude + '";"' + ne_altitude + '";"' + fe_altitude + '";"' + ne_sitename + '";"' + fe_sitename + '";"' + ne_azimuth + '";"' + fe_azimuth + '";"' + ne_construction_height + '";"' + fe_construction_height + '";"' + '"15";"Default_Radio";"0";"Default_Feeder";"Low";"Default_Antenna"' 
+yieldUnescaped '"' + tnuTsdNumber + '";"' + tnuTsdNumber + '";"' + ne_longitude + '";"' + fe_longitude + '";"' + ne_latitude + '";"' + fe_latitude + '";"' + ne_altitude + '";"' + fe_altitude + '";"' + ne_sitename + '";"' + fe_sitename + '";"' + ne_azimuth + '";"' + fe_azimuth + '";"' + ne_suspension_height_antennas + '";"' + fe_suspension_height + '";"' + '"15";"Default_Radio";"0";"Default_Feeder";"Low";"Default_Antenna"' 
 '''
 
 def config = new TemplateConfiguration()
