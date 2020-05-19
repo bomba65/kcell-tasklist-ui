0ea90c9924ac95444551e7f16aa4c0a7a29d6435
KurmetK <kurmetkopeyev@mail.ru>
2020-05-19 14:23:02 +0600

    sumbit not real error response

--- kcell-process-app/src/main/java/kz/kcell/bpm/tnuTsd/SendDataToAssets.java
+++ kcell-process-app/src/main/java/kz/kcell/bpm/tnuTsd/SendDataToAssets.java
@@ -4,14 +4,22 @@ import lombok.extern.java.Log;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.ExecutionListener;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.camunda.bpm.engine.delegate.BpmnError;
+
+import java.util.Random;
 
 @Log
 public class SendDataToAssets implements JavaDelegate {
 
     @Override
     public void execute(DelegateExecution execution) {
-
         log.info("sending data to Assets");
 
+        String[] list = {"400", "401", "403", "404", "500"};
+        Random r = new Random();
+
+        String errorMessage = list[r.nextInt(list.length)];
+
+        throw new BpmnError("error", errorMessage);
     }
 }
