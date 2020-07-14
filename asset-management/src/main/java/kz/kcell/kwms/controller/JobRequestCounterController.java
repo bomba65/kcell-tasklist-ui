d6d88e62d1557935c51ace2cfa96e4c0687893f8
Maxat Zhuniskhanov <zhmaksat@gmail.com>
2020-07-14 17:31:16 +0600

    Leasing: coontract counter starts from 5000

--- asset-management/src/main/java/kz/kcell/kwms/controller/JobRequestCounterController.java
+++ asset-management/src/main/java/kz/kcell/kwms/controller/JobRequestCounterController.java
@@ -46,7 +46,7 @@ public class JobRequestCounterController {
     @ResponseBody
     public String getNextRollourCounter(@PathVariable("counterId") String counterId) {
         JobRequestCounter jobRequestCounter = (JobRequestCounter) em.createNativeQuery(
-                "insert into jobrequest_counter as d (id,value) values(?1,0001) on conflict(id) do update set value = d.value + 1 returning *\n",
+                "insert into jobrequest_counter as d (id,value) values(?1,5000) on conflict(id) do update set value = d.value + 1 returning *\n",
                 JobRequestCounter.class)
                 .setParameter(1, counterId).getSingleResult();
 
