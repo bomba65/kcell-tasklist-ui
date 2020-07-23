5deff96fd36cae232a3f589949f8f09563c806f3
Maxat Zhuniskhanov <zhmaksat@gmail.com>
2020-07-23 22:22:15 +0600

    DP-557, added group to notification receiver list

--- kcell-process-app/src/main/resources/PBX/notification_addresses.groovy
+++ kcell-process-app/src/main/resources/PBX/notification_addresses.groovy
@@ -1,8 +1,17 @@
 import org.camunda.bpm.engine.delegate.DelegateExecution
 
+import java.util.stream.Collectors
+
 def getSalesRepresentativeEmails(DelegateExecution execution) {
+    def identityService = execution.processEngineServices.identityService
+    def list = identityService.createUserQuery().memberOfGroup("delivery_transmission_network_operations").list().stream()
+            .map{it.email}
+            .filter{it != null && !it.empty}
+            .collect(Collectors.toSet())
+
     def ts = execution.getVariable("technicalSpecifications").unwrap()
-    def result = ts.get('technicalEmail').asText()
+    list.add(ts.get('technicalEmail').asText());
+    def result = list.stream().collect(Collectors.joining(","))
     result
 }
 
