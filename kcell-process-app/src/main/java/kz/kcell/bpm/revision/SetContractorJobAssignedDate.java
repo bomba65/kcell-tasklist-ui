2277725f4195fe487c6fc96ca8796eb8363e7544
Nurlan Muldashev <nurlan@muldashev.kz>
2020-08-20 17:54:59 +0600

    KWMS-1040: validityDate, requestedDate

--- kcell-process-app/src/main/java/kz/kcell/bpm/revision/SetContractorJobAssignedDate.java
+++ kcell-process-app/src/main/java/kz/kcell/bpm/revision/SetContractorJobAssignedDate.java
@@ -3,14 +3,16 @@ package kz.kcell.bpm.revision;
 import org.camunda.bpm.engine.delegate.DelegateTask;
 import org.camunda.bpm.engine.delegate.TaskListener;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 public class SetContractorJobAssignedDate implements TaskListener {
 
     @Override
     public void notify(DelegateTask delegateTask) {
-
-        if(delegateTask.hasVariable("contractorJobAssignedDate")){
+        if (delegateTask.hasVariable("contractorJobAssignedDate")) {
             Date contractorJobAssignedDate = (Date) delegateTask.getVariable("contractorJobAssignedDate");
 
             long afterModifyList = delegateTask.getProcessEngineServices()
@@ -21,29 +23,50 @@ public class SetContractorJobAssignedDate implements TaskListener {
                 .finishedAfter(contractorJobAssignedDate)
                 .count();
 
-            if(afterModifyList > 0){
-                delegateTask.setVariable("contractorJobAssignedDate", new Date());
+
+            if (afterModifyList > 0) {
+                if ((Long) delegateTask.getVariable("contractor") == 4L) {
+                    try {
+                        delegateTask.setVariable("contractorJobAssignedDate", new SimpleDateFormat("dd.MM.yyyy").parse("30.06.2020"));
+                        delegateTask.setVariable("validityDate", new SimpleDateFormat("dd.MM.yyyy").parse("30.06.2020"));
+                        delegateTask.setVariable("requestedDate", new SimpleDateFormat("dd.MM.yyyy").parse("30.06.2020"));
+                    } catch (ParseException e) {
+                        delegateTask.setVariable("contractorJobAssignedDate", new Date());
+                    }
+                } else {
+                    delegateTask.setVariable("contractorJobAssignedDate", new Date());
+                }
             }
         } else {
-            delegateTask.setVariable("contractorJobAssignedDate", new Date());
+            if ((Long) delegateTask.getVariable("contractor") == 4L) {
+                try {
+                    delegateTask.setVariable("contractorJobAssignedDate", new SimpleDateFormat("dd.MM.yyyy").parse("30.06.2020"));
+                    delegateTask.setVariable("validityDate", new SimpleDateFormat("dd.MM.yyyy").parse("30.06.2020"));
+                    delegateTask.setVariable("requestedDate", new SimpleDateFormat("dd.MM.yyyy").parse("30.06.2020"));
+                } catch (ParseException e) {
+                    delegateTask.setVariable("contractorJobAssignedDate", new Date());
+                }
+            } else {
+                delegateTask.setVariable("contractorJobAssignedDate", new Date());
+            }
         }
 
         String mainContract = String.valueOf(delegateTask.getVariable("mainContract"));
-        if("Roll-out".equals(mainContract)){
-            if(!delegateTask.hasVariable("rolloutRO1")){
+        if ("Roll-out".equals(mainContract)) {
+            if (!delegateTask.hasVariable("rolloutRO1")) {
                 delegateTask.setVariable("rolloutRO1", "false");
             }
-            if(!delegateTask.hasVariable("rolloutRO2")){
+            if (!delegateTask.hasVariable("rolloutRO2")) {
                 delegateTask.setVariable("rolloutRO2", "false");
             }
-            if(!delegateTask.hasVariable("rolloutRO3")){
+            if (!delegateTask.hasVariable("rolloutRO3")) {
                 delegateTask.setVariable("rolloutRO3", "false");
             }
-            if(!delegateTask.hasVariable("rolloutRO4")){
+            if (!delegateTask.hasVariable("rolloutRO4")) {
                 delegateTask.setVariable("rolloutRO4", "false");
             }
             String workTitlesForSearch = delegateTask.getVariable("workTitlesForSearch").toString();
-            if(workTitlesForSearch.contains("13.")){
+            if (workTitlesForSearch.contains("13.")) {
                 delegateTask.setVariable("contains13Work", true);
             } else {
                 delegateTask.setVariable("contains13Work", false);
