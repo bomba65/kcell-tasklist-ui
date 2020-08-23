bffce8624fd13a811b0d0bb52c62604b665254b4
Maxat Zhuniskhanov <zhmaksat@gmail.com>
2020-08-23 18:24:35 +0600

    Revision PR: cancel process if works change 2

--- kcell-process-app/src/main/java/kz/kcell/bpm/revision/CancelPrProcess.java
+++ kcell-process-app/src/main/java/kz/kcell/bpm/revision/CancelPrProcess.java
@@ -1,29 +1,32 @@
 package kz.kcell.bpm.revision;
 
+import lombok.extern.java.Log;
+import org.camunda.bpm.engine.RuntimeService;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.ExecutionListener;
 import org.camunda.bpm.engine.runtime.Execution;
+import org.camunda.bpm.engine.runtime.ProcessInstance;
 
 import java.util.List;
 
+@Log
 public class CancelPrProcess implements ExecutionListener {
-
 
     @Override
     public void notify(DelegateExecution execution) throws Exception {
 
-        List<Execution> processInstances = execution
+        RuntimeService runtimeService = execution
             .getProcessEngineServices()
-            .getRuntimeService()
-            .createExecutionQuery()
+            .getRuntimeService();
+
+        List<ProcessInstance> processInstances = runtimeService
+            .createProcessInstanceQuery()
             .processDefinitionKey("CreatePR")
             .processInstanceBusinessKey("PR-" + execution.getProcessBusinessKey())
             .list();
 
-        for(Execution prExecution: processInstances){
-            execution
-                .getProcessEngineServices()
-                .getRuntimeService().setVariable(prExecution.getProcessInstanceId(), "prCreationInProgress", false);
+        for(ProcessInstance processInstance: processInstances){
+            runtimeService.setVariable(processInstance.getId(), "prCreationInProgress", "false");
         }
     }
 }
