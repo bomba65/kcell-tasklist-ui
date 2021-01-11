c9ae2652015b4cd9d49ddb76693a1a4d7b6aeee4
Baurzhan <baur.tass@gmail.com>
2021-01-11 23:42:46 +0600

    KWMS-1291: fix message body

--- kcell-process-app/src/main/resources/hopDelete/SendNotificationText.groovy
+++ kcell-process-app/src/main/resources/hopDelete/SendNotificationText.groovy
@@ -2,11 +2,14 @@ package revision
 
 import groovy.text.markup.MarkupTemplateEngine
 import groovy.text.markup.TemplateConfiguration
+import org.camunda.bpm.engine.impl.util.json.JSONObject
 
 import java.text.SimpleDateFormat
 
+JSONObject tsd = new JSONObject(execution.getVariable("selectedTsd").toString());
+String ne_site_name = tsd.getJSONObject("nearend_id").getString("site_name");
+String fe_site_name = tsd.getJSONObject("farend_id").getString("site_name");
 String regionName = execution.getVariable("regionName").toString();
-String site_name = execution.getVariable("site_name").toString();
 String cancelTsdNumber = execution.getVariable("cancel-tsdNumber").toString();
 String starter = execution.getVariable("starter").toString();
 
@@ -18,7 +21,7 @@ def date = formatDateTime.format(calendar.getTime())
 
 def binding = [
     "regionName" : regionName,
-    "siteName" : site_name,
+    "siteName" : ne_site_name + "-" + fe_site_name,
     "number" : cancelTsdNumber,
     "starter" : starter,
     "date" : date,
@@ -35,12 +38,6 @@ html(lang:'en') {
     body {
         p("В рамках рассмотрения заявок по <b>Cancel TSD</b> в системе <b>Kcell Workflow</b> был запущен процесс по <b>отмене TSD</b> по заявке <b>#" + number + "</b> ожидающая Вашего участия.")
         newLine()
-        p {
-            yield 'Для просмотра заявки Вам необходимо пройти по следующей '
-            b {
-                a(href: "https://flow.kcell.kz", "ссылке:")
-            }
-        }
         table(class:"table", style:"font-size:12px;") {
             tr {
                 td (width:"40%", style:"border:1px solid", "Процесс")
@@ -71,7 +68,12 @@ html(lang:'en') {
             }     
         }  
         newLine()
-        p("Открыть Kcell Workflow Вы можете пройдя по следующей ссылке: https://flow.kcell.kz")
+        p {
+            yield "Открыть Kcell Workflow Вы можете пройдя по следующей ссылке: "
+            b {
+                a(href: "https://flow.kcell.kz", "https://flow.kcell.kz")
+            }
+        } 
         newLine()
         p {
             yield "При возникновении каких-либо проблем при работе с системой, а также, пожеланий и предложений, отправьте пожалуйста письмо в "
