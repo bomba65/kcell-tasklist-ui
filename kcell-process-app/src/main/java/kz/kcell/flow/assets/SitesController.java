8ba40d1959f2f485bbd50eb16f3f7d76c630a86f
Maxat Zhuniskhanov <zhmaksat@gmail.com>
2021-01-18 14:28:34 +0600

    KWMS-1315, proxy assets methods for ui in revision, tsd, dismantle and replacement start pages

---
+++ kcell-process-app/src/main/java/kz/kcell/flow/assets/SitesController.java
@@ -0,0 +1,90 @@
+package kz.kcell.flow.assets;
+
+import lombok.extern.java.Log;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.ssl.SSLContextBuilder;
+import org.apache.http.util.EntityUtils;
+import org.camunda.bpm.engine.IdentityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.net.URLEncoder;
+
+@RestController
+@RequestMapping("/sites")
+@Log
+public class SitesController {
+
+    private final String assetsUrl;
+
+    @Autowired
+    private IdentityService identityService;
+
+    private SitesController(@Value("${asset.url:https://asset.test-flow.kcell.kz}") String assetsUrl){
+        this.assetsUrl = assetsUrl;
+    }
+
+    @RequestMapping(value = "/name/contains/{name}", method = RequestMethod.GET)
+    @ResponseBody
+    public ResponseEntity<String> siteContainsName(@PathVariable("name") String name) throws Exception {
+
+        if (identityService.getCurrentAuthentication() == null || identityService.getCurrentAuthentication().getUserId() == null) {
+            log.warning("No user logged in");
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);
+        }
+
+        SSLContextBuilder builder = new SSLContextBuilder();
+        builder.loadTrustMaterial(null, new TrustSelfSignedStrategy());
+        SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(builder.build());
+        CloseableHttpClient httpclient = HttpClients.custom().setSSLSocketFactory(sslsf).build();
+
+        HttpGet httpGet = new HttpGet(assetsUrl + "/asset-management/sites/name/contains/" + name);
+        HttpResponse httpResponse = httpclient.execute(httpGet);
+
+        HttpEntity entity = httpResponse.getEntity();
+        String content = EntityUtils.toString(entity);
+
+        if (httpResponse.getStatusLine().getStatusCode() < 200 || httpResponse.getStatusLine().getStatusCode() >= 300) {
+            throw new RuntimeException(assetsUrl + " site contains name = " + name + " returns code " + httpResponse.getStatusLine().getStatusCode());
+        }
+
+        return ResponseEntity.ok(content);
+    }
+
+    @RequestMapping(value = "/id/{id}", method = RequestMethod.GET)
+    @ResponseBody
+    public ResponseEntity<String> siteContainsName(@PathVariable("id") Long id) throws Exception {
+
+        if (identityService.getCurrentAuthentication() == null || identityService.getCurrentAuthentication().getUserId() == null) {
+            log.warning("No user logged in");
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);
+        }
+
+        SSLContextBuilder builder = new SSLContextBuilder();
+        builder.loadTrustMaterial(null, new TrustSelfSignedStrategy());
+        SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(builder.build());
+        CloseableHttpClient httpclient = HttpClients.custom().setSSLSocketFactory(sslsf).build();
+
+        HttpGet httpGet = new HttpGet(assetsUrl + "/asset-management/sites/id/" + id);
+        HttpResponse httpResponse = httpclient.execute(httpGet);
+
+        HttpEntity entity = httpResponse.getEntity();
+        String content = EntityUtils.toString(entity);
+
+        if (httpResponse.getStatusLine().getStatusCode() < 200 || httpResponse.getStatusLine().getStatusCode() >= 300) {
+            throw new RuntimeException(assetsUrl + " site getby id = " + id + " returns code " + httpResponse.getStatusLine().getStatusCode());
+        }
+
+        return ResponseEntity.ok(content);
+    }
+}
