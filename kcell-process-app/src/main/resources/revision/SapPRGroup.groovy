5f4cb43231d76e1b83965d4cb15aebc6ce2c18c2
ErbolTokpaev <erbol.tokpaev@gmail.com>
2021-01-21 00:20:02 +0600

    KWMS-1318: added Begaly.KoKin@kcell.kz

--- kcell-process-app/src/main/resources/revision/SapPRGroup.groovy
+++ kcell-process-app/src/main/resources/revision/SapPRGroup.groovy
@@ -8,23 +8,28 @@ import java.util.stream.Collectors
 def getUserEmail(DelegateExecution execution) {
     def identityService = execution.processEngineServices.identityService
     String reason = execution.getVariable("reason").toString()
-    def groupName = "";
+    def groupName = ""
+    def result = ""
+    if(reason.equals("5")) {
+        result = "Begaly.Kokin@kcell.kz";
+    } else {
+        if(reason.equals("1")){
+            groupName = "hq_sap_specialist_optimization"
+        } else if(reason.equals("2")){
+            groupName = "hq_sap_specialist_transmission"
+        } else if(reason.equals("3")){
+            groupName = "hq_sap_specialist_infrastructure"
+        } else if(reason.equals("4")){
+            groupName = "hq_sap_specialist_operation"
+        }
 
-    if(reason.equals("1")){
-        groupName = "hq_sap_specialist_optimization"
-    } else if(reason.equals("2")){
-        groupName = "hq_sap_specialist_transmission"
-    } else if(reason.equals("3")){
-        groupName = "hq_sap_specialist_infrastructure"
-    } else if(reason.equals("4")){
-        groupName = "hq_sap_specialist_operation"
+        def userList = identityService.createUserQuery().memberOfGroup(groupName).list().stream()
+                .map{it.getEmail()}
+                .filter{it != null && !it.isEmpty()}
+                .collect(Collectors.toSet())
+        result = userList.stream().collect(Collectors.joining(","))
     }
 
-    def userList = identityService.createUserQuery().memberOfGroup(groupName).list().stream()
-            .map{it.getEmail()}
-            .filter{it != null && !it.isEmpty()}
-            .collect(Collectors.toSet())
-    def result = userList.stream().collect(Collectors.joining(","))
     println 'get users:'
     println result
     result
