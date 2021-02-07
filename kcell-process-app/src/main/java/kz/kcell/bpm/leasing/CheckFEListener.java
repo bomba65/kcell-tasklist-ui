925b4d45460b9f7d3eb23a41549941c3831471ae
Nurlan Muldashev <nurlan@Nurlans-MacBook-Pro.local>
2021-02-07 22:20:54 +0600

    KWMS-1341: added logic to change region from NCP starts with 11

--- kcell-process-app/src/main/java/kz/kcell/bpm/leasing/CheckFEListener.java
+++ kcell-process-app/src/main/java/kz/kcell/bpm/leasing/CheckFEListener.java
@@ -7,6 +7,11 @@ public class CheckFEListener implements TaskListener {
     @Override
     public void notify(DelegateTask delegateTask) {
         String siteRegion = delegateTask.getVariable("siteRegion").toString();
+        String ncpID = delegateTask.getVariable("ncpID").toString();
+        if (ncpID != null && ncpID.startsWith("11") && !siteRegion.equals("Astana")) {
+            siteRegion = "Astana";
+            delegateTask.setVariable("siteRegion", siteRegion);
+        }
         delegateTask.addCandidateGroup(siteRegion + "_transmission");
     }
 }
