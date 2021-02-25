f91b49722adfb1e57b1dee8711217b41ff0b4e28
Baurzhan <baur.tass@gmail.com>
2021-02-25 11:50:37 +0600

    KWMS-1352: add assignment expression

--- kcell-process-app/src/main/java/kz/kcell/bpm/assignments/RegionHqLeasingAssignmentHandler.java
+++ kcell-process-app/src/main/java/kz/kcell/bpm/assignments/RegionHqLeasingAssignmentHandler.java
@@ -2,16 +2,23 @@ package kz.kcell.bpm.assignments;
 
 import org.camunda.bpm.engine.delegate.DelegateTask;
 import org.camunda.bpm.engine.delegate.TaskListener;
+import org.camunda.bpm.engine.impl.util.json.JSONObject;
 
 public class RegionHqLeasingAssignmentHandler implements TaskListener {
     @Override
     public void notify(DelegateTask delegateTask) {
         String siteRegion = delegateTask.getVariable("siteRegion").toString();
         String siteObjectType = delegateTask.getVariable("siteObjectType").toString();
-        if (siteObjectType.equals("national")) {
+        JSONObject candidate = new JSONObject(delegateTask.getVariable("candidate").toString());
+
+        if (candidate.has("constructionType") && candidate.getJSONObject("constructionType").getString("id").equals("141")) {
             delegateTask.addCandidateGroup("hq_leasing");
         } else {
-            delegateTask.addCandidateGroup(siteRegion + "_leasing");
+            if (siteObjectType.equals("national")) {
+                delegateTask.addCandidateGroup("hq_leasing");
+            } else {
+                delegateTask.addCandidateGroup(siteRegion + "_leasing");
+            }
         }
     }
 }
--- kcell-process-app/src/main/java/kz/kcell/bpm/leasing/ConfirmPermissionListener.java
+++ kcell-process-app/src/main/java/kz/kcell/bpm/leasing/ConfirmPermissionListener.java
@@ -2,16 +2,24 @@ package kz.kcell.bpm.leasing;
 
 import org.camunda.bpm.engine.delegate.DelegateTask;
 import org.camunda.bpm.engine.delegate.TaskListener;
+import org.camunda.bpm.engine.impl.util.json.JSONObject;
 
 public class ConfirmPermissionListener implements TaskListener {
     @Override
     public void notify(DelegateTask delegateTask) {
         String siteRegion = delegateTask.getVariable("siteRegion").toString();
         String ncpID = delegateTask.getVariable("ncpID").toString();
+
+        JSONObject candidate = new JSONObject(delegateTask.getVariable("candidate").toString());
+
         if (ncpID != null && ncpID.startsWith("11") && !siteRegion.equals("astana")) {
             siteRegion = "astana";
             delegateTask.setVariable("siteRegion", siteRegion);
         }
-        delegateTask.addCandidateGroup(siteRegion + "_leasing");
+        if (candidate.has("constructionType") && candidate.getJSONObject("constructionType").getString("id").equals("141")) {
+            delegateTask.addCandidateGroup("hq_leasing");
+        } else {
+            delegateTask.addCandidateGroup(siteRegion + "_leasing");
+        }
     }
 }
