e2f88902035c832366caa810baf0fc5e0fd12c84
Maxat Zhuniskhanov <zhmaksat@gmail.com>
2021-04-21 11:20:01 +0600

    KWMS-1367, 6 and 7 pints

--- kcell-process-app/src/main/java/kz/kcell/bpm/assignments/tnuTsd/RegionEngineerErrorHandler.java
+++ kcell-process-app/src/main/java/kz/kcell/bpm/assignments/tnuTsd/RegionEngineerErrorHandler.java
@@ -12,12 +12,12 @@ public class RegionEngineerErrorHandler implements TaskListener {
     public void notify(DelegateTask delegateTask) {
         String region_name = delegateTask.getVariable("region_name").toString();
         Map<String, String> map = new HashMap<>();
-        map.put("alm", "Andrey.Ivanov@kcell.kz,Nikolay.Ustinov@kcell.kz");
-        map.put("astana", "Alexandr.Galat@kcell.kz,Marat.Abdin@kcell.kz,Nikolay.Ustinov@kcell.kz");
-        map.put("nc", "Alexandr.Galat@kcell.kz,Marat.Abdin@kcell.kz,Nikolay.Ustinov@kcell.kz");
-        map.put("east", "Nikolay.Ustinov@kcell.kz");
-        map.put("south", "Samat.Dautov@kcell.kz,Nikolay.Ustinov@kcell.kz");
-        map.put("west", "Nikolay.Ustinov@kcell.kz");
+        map.put("alm", "Andrey.Ivanov@kcell.kz,Nikolay.Ustinov@kcell.kz,Sheraly.Tolymbekov@kcell.kz");
+        map.put("astana", "Alexandr.Galat@kcell.kz,Marat.Abdin@kcell.kz,Nikolay.Ustinov@kcell.kz,Sheraly.Tolymbekov@kcell.kz");
+        map.put("nc", "Alexandr.Galat@kcell.kz,Marat.Abdin@kcell.kz,Nikolay.Ustinov@kcell.kz,Sheraly.Tolymbekov@kcell.kz");
+        map.put("east", "Nikolay.Ustinov@kcell.kz,Sheraly.Tolymbekov@kcell.kz");
+        map.put("south", "Sheraly.Tolymbekov@kcell.kz,Nikolay.Ustinov@kcell.kz");
+        map.put("west", "Nikolay.Ustinov@kcell.kz,Sheraly.Tolymbekov@kcell.kz");
 
         if(map.get(region_name).contains(",")){
             for(String username: map.get(region_name).split(",")){
