149da5bdcef3dda55b86b0b40c519731db37c680
ablAzhidin <azhidinov@mail.ru>
2022-12-09 17:38:01 +0600

    feature KWMS-1508 add groups based on reason for validate TR

--- kcell-process-app/src/main/java/kz/kcell/bpm/assignments/ValidateTrByCenterByGroupHandler.java
+++ kcell-process-app/src/main/java/kz/kcell/bpm/assignments/ValidateTrByCenterByGroupHandler.java
@@ -17,9 +17,9 @@ public class ValidateTrByCenterByGroupHandler implements TaskListener {
 
         if("2022Work-agreement".equals(mainContract)){
             if (reason.equals("4")) {
-                delegateTask.addCandidateGroup("hq_operation_approve");
+                delegateTask.addCandidateGroup("hq_operation_tr");
             } else if (Arrays.asList("1", "2", "3", "5").contains(reason)){
-                delegateTask.addCandidateGroup("hq_development_approve");
+                delegateTask.addCandidateGroup("hq_development_tr");
             }
         }else {
             if (group != null) {
