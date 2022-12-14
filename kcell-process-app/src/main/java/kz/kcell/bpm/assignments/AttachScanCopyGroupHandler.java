1c7d50cca9a93287e73853d7a624d2ef2cf88416
ablAzhidin <azhidinov@mail.ru>
2022-12-14 15:41:46 +0600

    fix KWMS-1548

---
+++ kcell-process-app/src/main/java/kz/kcell/bpm/assignments/AttachScanCopyGroupHandler.java
@@ -0,0 +1,13 @@
+package kz.kcell.bpm.assignments;
+
+import org.camunda.bpm.engine.delegate.DelegateTask;
+import org.camunda.bpm.engine.delegate.TaskListener;
+
+public class AttachScanCopyGroupHandler implements TaskListener {
+
+    @Override
+    public void notify(DelegateTask delegateTask) {
+        String siteRegion = delegateTask.getVariable("siteRegion").toString();
+        delegateTask.addCandidateGroup(siteRegion + "_engineer");
+    }
+}
