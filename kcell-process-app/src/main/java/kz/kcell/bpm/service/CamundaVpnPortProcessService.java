ade05aae7dabb326ee1442fbe4012f93c59bc2d0
y.estraikh <y.estraikh@p-s.kz>
2023-05-16 13:47:50 +0600

    feature KWMS-1753 add search for VPN Port process

---
+++ kcell-process-app/src/main/java/kz/kcell/bpm/service/CamundaVpnPortProcessService.java
@@ -0,0 +1,196 @@
+package kz.kcell.bpm.service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.camunda.bpm.engine.HistoryService;
+import org.camunda.bpm.engine.RepositoryService;
+import org.camunda.bpm.engine.history.HistoricProcessInstance;
+import org.camunda.bpm.engine.history.HistoricProcessInstanceQuery;
+import org.camunda.bpm.engine.history.HistoricVariableInstance;
+import org.camunda.bpm.engine.impl.persistence.entity.ProcessDefinitionEntity;
+import org.camunda.spin.impl.json.jackson.JacksonJsonNode;
+import org.springframework.stereotype.Service;
+import spinjar.com.fasterxml.jackson.databind.JsonNode;
+
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class CamundaVpnPortProcessService {
+    private final HistoryService historyService;
+    private final RepositoryService repositoryService;
+
+    private final static ObjectMapper objectMapper = new ObjectMapper();
+
+    private final static String VPN_PORT_PROCESS = "VPN_Port_process";
+    private final static String VPN_PORT_AUTO_PROCESS = "VPN_Port_auto_process";
+    
+    public List<Map<String, String>> getVpnPortProcess(
+        String process,
+        String channel,
+        String requestType,
+        String currentActivity,
+        String priority,
+        String dateCreated,
+        String requestNumber,
+        String portId,
+        String vpnId
+    ) throws ParseException {
+        HistoricProcessInstanceQuery vpnPortProcessHistoryQuery = historyService.createHistoricProcessInstanceQuery().processDefinitionKey(VPN_PORT_PROCESS);
+        HistoricProcessInstanceQuery vpnPortAutoProcessHistoryQuery = historyService.createHistoricProcessInstanceQuery().processDefinitionKey(VPN_PORT_AUTO_PROCESS);
+
+        if (channel != null) {
+            vpnPortProcessHistoryQuery.variableValueEquals("channel", channel);
+            vpnPortAutoProcessHistoryQuery.variableValueEquals("channel", channel);
+        }
+
+        if (requestType != null) {
+            vpnPortProcessHistoryQuery.variableValueEquals("request_type", requestType);
+            vpnPortAutoProcessHistoryQuery.variableValueEquals("request_type", requestType);
+        }
+
+        if (priority != null) {
+            vpnPortProcessHistoryQuery.variableValueEquals("priority", priority);
+            vpnPortAutoProcessHistoryQuery.variableValueEquals("priority", priority);
+        }
+
+        if (requestNumber != null) {
+            vpnPortProcessHistoryQuery.processInstanceBusinessKey(requestNumber);
+            vpnPortAutoProcessHistoryQuery.processInstanceBusinessKey(requestNumber);
+        }
+
+        if (dateCreated != null) {
+            String[] stringDates = dateCreated.split(" - ");
+            Date beginDate = new SimpleDateFormat("dd.MM.yy").parse(stringDates[0]);
+            Date endDate = new Date(new SimpleDateFormat("dd.MM.yy").parse(stringDates[1]).getTime() + 24 * 60 * 60 * 1000);
+            vpnPortProcessHistoryQuery.startedAfter(beginDate);
+            vpnPortProcessHistoryQuery.startedBefore(endDate);
+            vpnPortAutoProcessHistoryQuery.startedAfter(beginDate);
+            vpnPortAutoProcessHistoryQuery.startedBefore(endDate);
+        }
+
+        if (currentActivity != null) {
+            vpnPortProcessHistoryQuery.activeActivityIdIn(currentActivity);
+            vpnPortAutoProcessHistoryQuery.activeActivityIdIn(currentActivity);
+        }
+
+        List<HistoricProcessInstance> historicProcessInstances = new ArrayList<>();
+        if (process != null && process.equals(VPN_PORT_PROCESS)) {
+            historicProcessInstances.addAll(vpnPortProcessHistoryQuery.list());
+        } else if (process != null && process.equals(VPN_PORT_AUTO_PROCESS)) {
+            historicProcessInstances.addAll(vpnPortAutoProcessHistoryQuery.list());
+        } else {
+            historicProcessInstances.addAll(vpnPortProcessHistoryQuery.list());
+            historicProcessInstances.addAll(vpnPortAutoProcessHistoryQuery.list());
+        }
+
+        if (portId != null) {
+            historicProcessInstances = historicProcessInstances.stream()
+                .filter(e -> getPortNumbers(e).contains(portId)).collect(Collectors.toList());
+        }
+
+        if (vpnId != null) {
+            historicProcessInstances = historicProcessInstances.stream()
+                .filter(e -> getVpnNumbers(e).contains(vpnId)).collect(Collectors.toList());
+        }
+
+        return historicProcessInstances.stream().map(processInstance -> {
+            Map<String, String> map = getVariables(processInstance);
+            map.put("processInstance", writeValueAsString(processInstance));
+            map.put("portId", writeValueAsString(getPortNumbers(processInstance)));
+            map.put("vpnId", writeValueAsString(getVpnNumbers(processInstance)));
+            map.put("taskActivities", writeValueAsString(historyService.createHistoricTaskInstanceQuery().processInstanceId(processInstance.getId()).list()));
+            return map;
+        }).collect(Collectors.toList());
+    }
+
+    public Map<String, String> getTaskList() {
+        String vpnPortProcessDefinitionId = historyService.createHistoricProcessInstanceQuery().processDefinitionKey(VPN_PORT_PROCESS)
+            .orderByProcessInstanceStartTime().desc().list().stream()
+            .map(HistoricProcessInstance::getProcessDefinitionId).findFirst().orElse(null);
+
+        String vpnPortAutoProcessDefinitionId = historyService.createHistoricProcessInstanceQuery().processDefinitionKey(VPN_PORT_AUTO_PROCESS)
+            .orderByProcessInstanceStartTime().desc().list().stream()
+            .map(HistoricProcessInstance::getProcessDefinitionId).findFirst().orElse(null);
+
+        Map<String, String> taskNames = new HashMap<>();
+        if (vpnPortProcessDefinitionId != null) {
+            taskNames.putAll(((ProcessDefinitionEntity) repositoryService.getProcessDefinition(vpnPortProcessDefinitionId))
+                .getTaskDefinitions().entrySet().stream()
+                .collect(Collectors.toMap(
+                    Map.Entry::getKey,
+                    e -> e.getValue().getNameExpression().getExpressionText()
+                )));
+        } else if (vpnPortAutoProcessDefinitionId != null) {
+            taskNames.putAll(((ProcessDefinitionEntity) repositoryService.getProcessDefinition(vpnPortAutoProcessDefinitionId))
+                .getTaskDefinitions().entrySet().stream()
+                .collect(Collectors.toMap(
+                    Map.Entry::getKey,
+                    e -> e.getValue().getNameExpression().getExpressionText()
+                )));
+        }
+
+        return taskNames;
+    }
+
+    private List<String> getVpnNumbers(HistoricProcessInstance processInstance) {
+        List<HistoricVariableInstance> vpnVars = historyService.createHistoricVariableInstanceQuery()
+            .processInstanceId(processInstance.getId())
+            .variableNameLike("%Services").list();
+        return vpnVars.stream()
+            .map(vpnVar -> ((JacksonJsonNode) vpnVar.getValue())).flatMap(node -> node.elements().stream())
+            .map(e -> ((JacksonJsonNode) e).unwrap().findValue("vpn_number"))
+            .filter(Objects::nonNull).map(JsonNode::toString).map(s -> s.replace("\"", "")).collect(Collectors.toList());
+    }
+
+    private Set<String> getPortNumbers(HistoricProcessInstance processInstance) {
+        List<HistoricVariableInstance> vpnVars = historyService.createHistoricVariableInstanceQuery()
+            .processInstanceId(processInstance.getId())
+            .variableNameLike("%Services").list();
+        Set<String> set = vpnVars.stream()
+            .map(vpnVar -> ((JacksonJsonNode) vpnVar.getValue())).flatMap(node -> node.elements().stream())
+            .map(e -> ((JacksonJsonNode) e).unwrap().findValue("port_id") != null ?
+                ((JacksonJsonNode) e).unwrap().findValue("port_id") : ((JacksonJsonNode) e).unwrap().findValue("port_number"))
+            .filter(Objects::nonNull).map(JsonNode::toString).map(s -> s.replace("\"", "")).collect(Collectors.toSet());
+
+        List<HistoricVariableInstance> portVars = historyService.createHistoricVariableInstanceQuery()
+            .processInstanceId(processInstance.getId())
+            .variableNameLike("%Ports").list();
+        set.addAll(portVars.stream()
+            .map(portVar -> ((JacksonJsonNode) portVar.getValue())).flatMap(node -> node.elements().stream())
+            .map(e -> ((JacksonJsonNode) e).unwrap().findValue("port_id") != null ?
+                ((JacksonJsonNode) e).unwrap().findValue("port_id") : ((JacksonJsonNode) e).unwrap().findValue("port_number"))
+            .filter(Objects::nonNull).map(JsonNode::toString).map(s -> s.replace("\"", "")).collect(Collectors.toList()));
+
+        return set;
+    }
+
+    private <T> String writeValueAsString(T value) {
+        try {
+            return objectMapper.writeValueAsString(value);
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
+
+    private Map<String, String> getVariables(HistoricProcessInstance processInstance) {
+        return historyService.createHistoricVariableInstanceQuery()
+            .processInstanceId(processInstance.getId())
+            .list().stream().collect(Collectors.toMap(
+                HistoricVariableInstance::getName,
+                e -> e.getValue().toString(),
+                (oldValue, newValue) -> newValue
+            ));
+    }
+}
---
+++ kcell-tasklist-ui/js/directives/search/vpnPortSearch.js
@@ -0,0 +1,254 @@
+define(['../module'], function (module) {
+    'use strict';
+    module.directive('vpnPortSearch', ['$http', '$timeout', '$q', '$rootScope', 'exModal', 'toasty', function ($http, $timeout, $q, $rootScope, exModal, toasty) {
+        return {
+            restrict: 'E',
+            scope: false,
+            link: function (scope, el, attrs) {
+                $http.get('/camunda/camunda-process/vpn-port-process/task-list').then(response => {
+                    scope.taskList = response.data;
+                })
+
+                scope.formData = {
+                    process: undefined,
+                    channel: undefined,
+                    requestType: undefined,
+                    currentActivity: undefined,
+                    priority: undefined,
+                    dateCreated: undefined,
+                    requestNumber: undefined,
+                    portId: undefined,
+                    vpnId: undefined
+                }
+
+                var processList = [];
+
+                scope.search = function () {
+                    scope.setCurrentPage(1);
+
+                    var queryParams = Object.keys(scope.formData)
+                        .map(function (key) {
+                            var value = scope.formData[key];
+                            if (value !== null && value !== undefined && value !== '') {
+                                return encodeURIComponent(key) + '=' + encodeURIComponent(value);
+                            }
+                            return null;
+                        })
+                        .filter(function (param) {
+                            return param !== null;
+                        })
+                        .join('&');
+                    $http.get('/camunda/camunda-process/vpn-port-process?' + queryParams).then(response => {
+                        scope.processInstances = response.data;
+                        for (var i = 0; i < scope.processInstances.length; i++) {
+                            scope.processInstances[i].processInstance = JSON.parse(scope.processInstances[i].processInstance);
+                            scope.processInstances[i].dateCreated = new Date(scope.processInstances[i].processInstance.startTime).toLocaleDateString();
+
+                            scope.processInstances[i].vpnId = JSON.parse(scope.processInstances[i].vpnId).filter(e => e !== undefined && e !== 'null');
+                            scope.processInstances[i].portId = JSON.parse(scope.processInstances[i].portId).filter(e => e !== undefined && e !== 'null');
+
+                            scope.processInstances[i].vpnId = (scope.processInstances[i].vpnId.length > 0) ? scope.processInstances[i].vpnId.join(', ') : '';
+                            scope.processInstances[i].portId = (scope.processInstances[i].portId.length > 0) ? scope.processInstances[i].portId.join(', ') : '';
+
+                            scope.processInstances[i].currentActivities = JSON.parse(scope.processInstances[i].taskActivities)
+                                .filter(e => e.deleteReason === null);
+
+                            scope.processInstances[i].currentActivitiesNames = scope.processInstances[i].currentActivities.map(e => e.name).join(", ");
+                            scope.processInstances[i].currentActivitiesAssignees = scope.processInstances[i].currentActivities.map(e => e.assignee).join(", ");
+
+                            var currentTasksDates = scope.processInstances[i].currentActivities.map(e => e.startTime);
+                            var dateUpdated = currentTasksDates.length > 0 ? Math.max(...currentTasksDates) : undefined;
+                            scope.processInstances[i].dateUpdated = (dateUpdated !== undefined) ? new Date(dateUpdated).toLocaleDateString()
+                                : new Date(scope.processInstances[i].processInstance.endTime).toLocaleDateString();
+
+                            if (scope.processInstances[i].channel === 'VPN') {
+                                var servicesString = searchPropValue(scope.processInstances[i], "Services");
+                                if (servicesString) {
+                                    scope.processInstances[i].vpns = JSON.parse(servicesString)
+                                }
+                            } else {
+                                var portsString = searchPropValue(scope.processInstances[i], "Ports");
+                                if (portsString) {
+                                    scope.processInstances[i].ports = JSON.parse(portsString)
+                                }
+                            }
+
+                            scope.processInstances[i].inputTheProvidersNorFiles = scope.processInstances[i].inputTheProvidersNorFiles ? JSON.parse(scope.processInstances[i].inputTheProvidersNorFiles) : null;
+
+                            scope.processInstances[i].resolutions = scope.processInstances[i].resolutions ? JSON.parse(scope.processInstances[i].resolutions) : null;
+                        }
+
+                        scope.processInstances.sort((a, b) => b.processInstance.startTime - a.processInstance.startTime);
+                    })
+                };
+
+                scope.clearFilter = function () {
+                    scope.formData = {
+                        process: undefined,
+                        channel: undefined,
+                        requestType: undefined,
+                        currentActivity: undefined,
+                        priority: undefined,
+                        dateCreated: undefined,
+                        requestNumber: undefined,
+                        portId: undefined,
+                        vpnId: undefined
+                    }
+                }
+
+                scope.sortColumn = function (column) {
+                    if (scope.sortBy === column) {
+                        scope.reverse = !scope.reverse;
+                    } else {
+                        scope.sortBy = column;
+                        scope.reverse = false;
+                    }
+                };
+
+                scope.itemsPerPage = 20;
+                scope.currentPage = 1;
+
+                scope.$watch('processInstances', function () {
+                    if (!scope.processInstances) return;
+
+                    scope.totalPages = Math.ceil(scope.processInstances.length / scope.itemsPerPage);
+                    scope.pages = [];
+                    for (var i = 1; i <= scope.totalPages; i++) {
+                        scope.pages.push(i);
+                    }
+                    var startIndex = (scope.currentPage - 1) * scope.itemsPerPage;
+                    var endIndex = startIndex + scope.itemsPerPage;
+                    scope.pagedProcessInstances = [...scope.processInstances].slice(startIndex, endIndex);
+                });
+
+                scope.setCurrentPage = function (page) {
+                    if (page >= 1 && page <= scope.totalPages) {
+                        scope.currentPage = page;
+                    }
+                };
+
+                scope.$watchGroup(['currentPage', 'sortBy', 'reverse'], function () {
+                    if (!scope.processInstances) return;
+
+                    var startIndex = (scope.currentPage - 1) * scope.itemsPerPage;
+                    var endIndex = startIndex + scope.itemsPerPage;
+                    var sortedData = scope.processInstances.slice(0); // Copy the data array
+
+                    if (scope.sortBy) {
+                        sortedData.sort(function (a, b) {
+                            var valueA = getObjectPropertyValue(a, scope.sortBy);
+                            var valueB = getObjectPropertyValue(b, scope.sortBy);
+                            if (valueA < valueB) {
+                                return scope.reverse ? -1 : 1;
+                            } else if (valueA > valueB) {
+                                return scope.reverse ? 1 : -1;
+                            }
+                            return 0;
+                        });
+                    }
+
+                    scope.pagedProcessInstances = sortedData.slice(startIndex, endIndex);
+                });
+
+                function getObjectPropertyValue(obj, path) {
+                    var properties = path.split('.');
+                    var value = obj;
+                    for (var i = 0; i < properties.length; i++) {
+                        if (value && value.hasOwnProperty(properties[i])) {
+                            value = value[properties[i]];
+                        } else {
+                            value = null;
+                            break;
+                        }
+                    }
+                    return value;
+                }
+
+                function searchPropValue(obj, searchString) {
+                    for (var prop in obj) {
+                        if (prop.includes(searchString)) {
+                            return obj[prop];
+                        }
+                    }
+                    return undefined; // Return undefined if no matching property is found
+                }
+
+                scope.openProcessCardModal = function (process) {
+                    exModal.open({
+                        scope: {
+                            process: process,
+                            addressToString: scope.addressToString,
+                            downloadFile: scope.downloadFile,
+                            showHistory: $rootScope.showHistory
+                        },
+                        templateUrl: './js/partials/vpnPortProcessCardModal.html',
+                        size: 'hg'
+                    }).then(function (results) {
+                    });
+                }
+
+                scope.addressToString = function (address) {
+                    let result = "";
+                    if (address) {
+                        if (address.city_id) {
+                            if (address.city_id.district_id) {
+                                if (address.city_id.district_id.oblast_id) {
+                                    if (address.city_id.district_id.oblast_id.name) {
+                                        result += address.city_id.district_id.oblast_id.name + ' ';
+                                    }
+                                }
+                                if (address.city_id.district_id.name) {
+                                    result += address.city_id.district_id.name + ' ';
+                                }
+                            }
+                            if (address.city_id.name) {
+                                result += address.city_id.name + ' ';
+                            }
+                        }
+                        if (address.street) {
+                            result += address.street + ' ';
+                        }
+                        if (address.building) {
+                            result += address.building;
+                        }
+                    }
+
+                    return result;
+                }
+
+                scope.downloadFile = function(file) {
+                    $http({
+                        method: 'GET',
+                        url: '/camunda/uploads/get/' + file.path,
+                        transformResponse: []
+                    }).then(function (response) {
+                        document.getElementById('fileDownloadIframe').src = response.data;
+                    }, function (error) {
+                        console.log(error);
+                    });
+                };
+
+                scope.exportToExcel = function() {
+                    var worksheet = XLSX.utils.table_to_sheet(document.getElementById('excelTable'));
+                    var workbook = XLSX.utils.book_new();
+                    XLSX.utils.book_append_sheet(workbook, worksheet, 'Sheet1');
+                    var excelBuffer = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' });
+                    saveAsExcel(excelBuffer, 'data.xlsx');
+                };
+
+                function saveAsExcel(buffer, filename) {
+                    var blob = new Blob([buffer], { type: 'application/octet-stream' });
+                    saveAs(blob, filename);
+                }
+
+                function saveAs(blob, filename) {
+                    var link = document.createElement('a');
+                    link.href = URL.createObjectURL(blob);
+                    link.download = filename;
+                    link.click();
+                }
+            },
+            templateUrl: './js/directives/search/vpnPortSearch.html'
+        };
+    }]);
+});
