81dfd3270f55bfa7c0fc9f33bddbcca770e4f1c7
b.tursunkulov <@Zhakypali.Ayaulym>
2023-06-30 12:33:56 +0600

    feature KWMS-1801 Revision. Contract SAO 2023. User task Attach Material List (assignee to Subcontactor)

--- kcell-process-app/src/main/java/kz/kcell/bpm/assignments/ContractorAssignmentHandler.java
+++ kcell-process-app/src/main/java/kz/kcell/bpm/assignments/ContractorAssignmentHandler.java
@@ -35,26 +35,16 @@ public class ContractorAssignmentHandler implements TaskListener {
         String reason = delegateTask.getVariable("reason").toString();
         String mainContract = delegateTask.getVariable("mainContract").toString();
         String siteId = delegateTask.getVariable("siteName").toString();
-        if ("2022Work-agreement".equals(mainContract) || "technical_maintenance_services".equals(mainContract)) {
+        String oblastName = delegateTask.getVariable("oblastName").toString();
+
+        if ("2022Work-agreement".equals(mainContract)) {
             String siteIdFirstTwoDigits = siteId.substring(0, 2);
             if("99".contains(siteIdFirstTwoDigits)){
                 delegateTask.addCandidateGroup(siteRegion+"_contractor_"+contractorsTitle.get(contractor));
-            } else if ("07".equals(siteIdFirstTwoDigits)) {
-                if ("technical_maintenance_services".equals(mainContract)) {
-                    if (contractor.equals("7")) {
-                        delegateTask.addCandidateGroup("nc_contractor_logycom");
-                    } else if (contractor.equals("8")) {
-                        delegateTask.addCandidateGroup("alm_contractor_arlan");
-                    }
-                } else if (siteRegion.equals("nc")) {
-                    delegateTask.addCandidateGroup("nc_contractor_logycom");
-                }
+            } else if ("07".equals(siteIdFirstTwoDigits)&&siteRegion.equals("nc")) {
+                delegateTask.addCandidateGroup("nc_contractor_logycom");
             } else if ("03".contains(siteIdFirstTwoDigits)) {
-                if ("technical_maintenance_services".equals(mainContract)) {
-                    delegateTask.addCandidateGroup("alm_contractor_arlan");
-                } else {
-                    delegateTask.addCandidateGroup("alm_contractor_alta");
-                }
+                delegateTask.addCandidateGroup("alm_contractor_alta");
             } else if (Arrays.asList("00", "01", "04", "05", "06", "07").contains(siteIdFirstTwoDigits)) {
                 delegateTask.addCandidateGroup("alm_contractor_alta");
             } else if (Arrays.asList("10", "11", "12", "13").contains(siteIdFirstTwoDigits)) {
@@ -76,6 +66,28 @@ public class ContractorAssignmentHandler implements TaskListener {
             } else if (Arrays.asList("71", "72").contains(siteIdFirstTwoDigits)) {
                 delegateTask.addCandidateGroup("west_contractor_transtlc");
             }
+        } else if ("technical_maintenance_services".equals(mainContract)) {
+            String siteIdFirstTwoDigits = siteId.substring(0, 2);
+            if (Arrays.asList("00", "01", "04", "05", "06", "07").contains(siteIdFirstTwoDigits)&&("Алматинская область".equals(oblastName)||"Жамбылская область".equals(oblastName))) {
+                delegateTask.addCandidateGroup("alm_contractor_arlan");
+            } else if (Arrays.asList("04", "05", "06").contains(siteIdFirstTwoDigits)&&"Жетысуская область".equals(oblastName)) {
+                delegateTask.addCandidateGroup("alm_contractor_inter");
+            } else if ("07".equals(siteIdFirstTwoDigits)&&"Карагандинская область".equals(oblastName)) {
+                delegateTask.addCandidateGroup("nc_contractor_logycom");
+            } else if (Arrays.asList("10", "11", "12", "13", "14", "15", "16", "17", "21", "22", "23", "24").contains(siteIdFirstTwoDigits)) {
+                delegateTask.addCandidateGroup("nc_contractor_logycom");
+            } else if (Arrays.asList("31", "32", "33", "34", "35", "36").contains(siteIdFirstTwoDigits)) {
+                delegateTask.addCandidateGroup("east_contractor_logycom");
+            } else if (Arrays.asList("43", "44", "45", "46").contains(siteIdFirstTwoDigits)) {
+                delegateTask.addCandidateGroup("south_contractor_arlan");
+            } else if (Arrays.asList("40","41", "42", "47", "48").contains(siteIdFirstTwoDigits)) {
+                delegateTask.addCandidateGroup("south_contractor_foresterhg");
+            } else if (Arrays.asList("71", "72").contains(siteIdFirstTwoDigits)) {
+                delegateTask.addCandidateGroup("west_contractor_logycom");
+            } else if (Arrays.asList("51", "52", "61", "62", "81", "82").contains(siteIdFirstTwoDigits)) {
+                delegateTask.addCandidateGroup("west_contractor_foresterhg");
+            }
+
         } else {
             if (contractor.equals("5")) {
                 if (reason.equals("5")) {
--- kcell-process-app/src/main/java/kz/kcell/bpm/assignments/RevisionAttachMaterialListGroupHandler.java
+++ kcell-process-app/src/main/java/kz/kcell/bpm/assignments/RevisionAttachMaterialListGroupHandler.java
@@ -4,6 +4,7 @@ import org.apache.commons.lang.ArrayUtils;
 import org.camunda.bpm.engine.delegate.DelegateTask;
 import org.camunda.bpm.engine.delegate.TaskListener;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -26,6 +27,8 @@ public class RevisionAttachMaterialListGroupHandler implements TaskListener {
     public void notify(DelegateTask delegateTask) {
         String siteRegion = delegateTask.getVariable("siteRegion").toString();
         String mainContract = delegateTask.getVariable("mainContract").toString();
+        String siteId = delegateTask.getVariable("siteName").toString();
+        String oblastName = delegateTask.getVariable("oblastName").toString();
 
         if ("2022Work-agreement".equals(mainContract)) {
             String siteName = delegateTask.getVariable("site_name").toString();
@@ -40,6 +43,28 @@ public class RevisionAttachMaterialListGroupHandler implements TaskListener {
                     .getKey();
                 delegateTask.addCandidateGroup(group);
             }
+        } else if ("technical_maintenance_services".equals(mainContract)) {
+            String siteIdFirstTwoDigits = siteId.substring(0, 2);
+            if (Arrays.asList("00", "01", "04", "05", "06", "07").contains(siteIdFirstTwoDigits)&&("Алматинская область".equals(oblastName)||"Жамбылская область".equals(oblastName))) {
+                delegateTask.addCandidateGroup("alm_contractor_arlan");
+            } else if (Arrays.asList("04", "05", "06").contains(siteIdFirstTwoDigits)&&"Жетысуская область".equals(oblastName)) {
+                delegateTask.addCandidateGroup("alm_contractor_inter");
+            } else if ("07".equals(siteIdFirstTwoDigits)&&"Карагандинская область".equals(oblastName)) {
+                delegateTask.addCandidateGroup("nc_contractor_logycom");
+            } else if (Arrays.asList("10", "11", "12", "13", "14", "15", "16", "17", "21", "22", "23", "24").contains(siteIdFirstTwoDigits)) {
+                delegateTask.addCandidateGroup("nc_contractor_logycom");
+            } else if (Arrays.asList("31", "32", "33", "34", "35", "36").contains(siteIdFirstTwoDigits)) {
+                delegateTask.addCandidateGroup("east_contractor_logycom");
+            } else if (Arrays.asList("43", "44", "45", "46").contains(siteIdFirstTwoDigits)) {
+                delegateTask.addCandidateGroup("south_contractor_arlan");
+            } else if (Arrays.asList("40","41", "42", "47", "48").contains(siteIdFirstTwoDigits)) {
+                delegateTask.addCandidateGroup("south_contractor_foresterhg");
+            } else if (Arrays.asList("71", "72").contains(siteIdFirstTwoDigits)) {
+                delegateTask.addCandidateGroup("west_contractor_logycom");
+            } else if (Arrays.asList("51", "52", "61", "62", "81", "82").contains(siteIdFirstTwoDigits)) {
+                delegateTask.addCandidateGroup("west_contractor_foresterhg");
+            }
+
         } else {
             delegateTask.addCandidateGroup(siteRegion + "_engineer");
         }
